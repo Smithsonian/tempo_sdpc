acf030a528d386c8661d2cdf6c0bc0eadd27ac1e
Ewan O'Sullivan <eosullivan@cfa.harvard.edu>
2014-08-05 14:05:52 -0400

    Removed obsolete interpol3 and interpold3 functions from m_interpol, plus minor tidying of m_sigma, m_triquad, mathcons.

--- L1_cloud/src/m_sigma.f90
+++ L1_cloud/src/m_sigma.f90
@@ -1,116 +1,119 @@
 module m_sigma
 
- interface sigma
-        module procedure r_sigma
-        module procedure r4_sigma
- end interface
+  interface sigma
+    module procedure r_sigma
+    module procedure r4_sigma
+  end interface
 
 contains
 
-     function  r_sigma(ar1, avg1, iprt) result(std)
+  function  r_sigma(ar1, avg1, iprt) result(std)
 
-     use m_avg
-     implicit none
+    use m_avg
+    implicit none
 
-!-------------------------------------------------------------------------
-!         NASA/GSFC, Data Assimilation Office, Code 910.3, GEOS/DAS      !
-!-------------------------------------------------------------------------
-!BOP
-!
-! !ROUTINE: sigma 
-!
-! !DESCRIPTION:  finds std dev of a vector (not a matrix)
-!
-! !CALLING SEQUENCE:
-!
-!        std = sigma(vectorin)
-!
-! !INPUT PARAMETERS:
-     real (KIND=8), dimension(:)  :: ar1
-!                       ar1 : input vector to find std deviation
-     real (KIND=8),    optional   :: avg1
-     integer, optional   :: iprt
-!
-! !OUTPUT PARAMETERS:
-     real (KIND=8)                :: std
-!                       std      :  std deviation
-!
-! !SEE ALSO:  m_avg
-!
-! !REVISION HISTORY:
-!
-!  21Oct97   M. Karki     Original code.
-!EOP
-!-------------------------------------------------------------------------
+    !-------------------------------------------------------------------------
+    !         NASA/GSFC, Data Assimilation Office, Code 910.3, GEOS/DAS      !
+    !-------------------------------------------------------------------------
+    !BOP
+    !
+    ! !ROUTINE: sigma 
+    !
+    ! !DESCRIPTION:  finds std dev of a vector (not a matrix)
+    !
+    ! !CALLING SEQUENCE:
+    !
+    !        std = sigma(vectorin)
+    !
+    ! !INPUT PARAMETERS:
+    real (KIND=8), dimension(:)  :: ar1
+    !                       ar1 : input vector to find std deviation
+    real (KIND=8),    optional   :: avg1
+    integer, optional            :: iprt
+    !
+    ! !OUTPUT PARAMETERS:
+    real (KIND=8)                :: std
+    !                       std      :  std deviation
+    !
+    ! !SEE ALSO:  m_avg
+    !
+    ! !REVISION HISTORY:
+    !
+    !  21Oct97   M. Karki     Original code.
+    !EOP
+    !-------------------------------------------------------------------------
 
-!! LOCAL PARAMETERS: none
+    !! LOCAL PARAMETERS: none
 
-! note this is the correct standard deviation when estimating
-! the mean
-!    std = sqrt(sum( (ar1-avg(ar1))**2) /float(size(ar1)-1) )
-! this is the function evaluated in the IDL sigma function
+    ! note this is the correct standard deviation when estimating
+    ! the mean
+    !    std = sqrt(sum( (ar1-avg(ar1))**2) /float(size(ar1)-1) )
+    ! this is the function evaluated in the IDL sigma function
     real (KIND=8) :: avgarg
 
-     avgarg=avg(ar1)
+    avgarg=avg(ar1)
 
-!     std = sqrt(sum( (ar1-avgarg)**2) /float(size(ar1)) )
-     std = sqrt(sum( (ar1-avgarg)**2) /real(size(ar1)) )
+    !     std = sqrt(sum( (ar1-avgarg)**2) /float(size(ar1)) )
+    std = sqrt(sum( (ar1-avgarg)**2) /real(size(ar1)) )
 
-     if (present(avg1)) avg1=avgarg
+    if (present(avg1)) avg1=avgarg
 
-     end function r_sigma
+  end function r_sigma
 
-     function  r4_sigma(ar1, avg1, iprt) result(std)
 
-     use m_avg
-     implicit none
 
-!-------------------------------------------------------------------------
-!         NASA/GSFC, Data Assimilation Office, Code 910.3, GEOS/DAS      !
-!-------------------------------------------------------------------------
-!BOP
-!
-! !ROUTINE: sigma 
-!
-! !DESCRIPTION:  finds std dev of a vector (not a matrix)
-!
-! !CALLING SEQUENCE:
-!
-!        std = sigma(vectorin)
-!
-! !INPUT PARAMETERS:
-     real (kind=4), dimension(:)  :: ar1
-!                       ar1 : input vector to find std deviation
-     real (kind=4),    optional   :: avg1
-     integer, optional   :: iprt
-!
-! !OUTPUT PARAMETERS:
-     real  (kind=4)               :: std
-!                       std      :  std deviation
-!
-! !SEE ALSO:  m_avg
-!
-! !REVISION HISTORY:
-!
-!  21Oct97   M. Karki     Original code.
-!EOP
-!-------------------------------------------------------------------------
 
-!! LOCAL PARAMETERS: none
+  function  r4_sigma(ar1, avg1, iprt) result(std)
 
-! note this is the correct standard deviation when estimating
-! the mean
-!    std = sqrt(sum( (ar1-avg(ar1))**2) /float(size(ar1)-1) )
-! this is the function evaluated in the IDL sigma function
+    use m_avg
+    implicit none
+
+    !-------------------------------------------------------------------------
+    !         NASA/GSFC, Data Assimilation Office, Code 910.3, GEOS/DAS      !
+    !-------------------------------------------------------------------------
+    !BOP
+    !
+    ! !ROUTINE: sigma 
+    !
+    ! !DESCRIPTION:  finds std dev of a vector (not a matrix)
+    !
+    ! !CALLING SEQUENCE:
+    !
+    !        std = sigma(vectorin)
+    !
+    ! !INPUT PARAMETERS:
+    real (kind=4), dimension(:)  :: ar1
+    !                       ar1 : input vector to find std deviation
+    real (kind=4),    optional   :: avg1
+    integer, optional            :: iprt
+    !
+    ! !OUTPUT PARAMETERS:
+    real  (kind=4)               :: std
+    !                       std      :  std deviation
+    !
+    ! !SEE ALSO:  m_avg
+    !
+    ! !REVISION HISTORY:
+    !
+    !  21Oct97   M. Karki     Original code.
+    !EOP
+    !-------------------------------------------------------------------------
+
+    !! LOCAL PARAMETERS: none
+
+    ! note this is the correct standard deviation when estimating
+    ! the mean
+    !    std = sqrt(sum( (ar1-avg(ar1))**2) /float(size(ar1)-1) )
+    ! this is the function evaluated in the IDL sigma function
     real (KIND=8) :: avgarg
 
-     avgarg=avg(ar1)
+    avgarg=avg(ar1)
 
-!     std = sqrt(sum( (ar1-avgarg)**2) /float(size(ar1)) )
-     std = sqrt(sum( (ar1-avgarg)**2) /real(size(ar1)) )
+    !     std = sqrt(sum( (ar1-avgarg)**2) /float(size(ar1)) )
+    std = sqrt(sum( (ar1-avgarg)**2) /real(size(ar1)) )
 
-     if (present(avg1)) avg1=avgarg
+    if (present(avg1)) avg1=avgarg
 
-     end function r4_sigma
+  end function r4_sigma
 
 end module m_sigma
--- L1_cloud/src/mathcons.f90
+++ L1_cloud/src/mathcons.f90
@@ -1,23 +1,23 @@
-       Module mathcons
-!-------------------------------------------------------------------------
-!         NASA/GSFC, Data Assimilation Office, Code 910.3, GEOS/DAS      !
-!-------------------------------------------------------------------------
-!!BOP
-!***********************************************************************
-!
-!  mathematical constants 
-!***********************************************************************
-!EOP
-!
-      implicit none
+Module mathcons
+  !-------------------------------------------------------------------------
+  !         NASA/GSFC, Data Assimilation Office, Code 910.3, GEOS/DAS      !
+  !-------------------------------------------------------------------------
+  !!BOP
+  !***********************************************************************
+  !
+  !  mathematical constants 
+  !***********************************************************************
+  !EOP
+  !
+  implicit none
 
-      real (KIND=8), parameter ::      pi = 3.14159265359 !   pi
-      real (KIND=8), parameter ::      deg2rad = PI / 180.0  !  convert degrees to radians
-      real (KIND=8), parameter ::      pid2 = PI / 2.0 !   pi/2
-      real (KIND=8), parameter ::      rad2deg = 180.0 / PI !  convert radians to degrees
-      real (KIND=8), parameter ::      twopi = 2 * pi
+  real (KIND=8), parameter :: pi = 3.14159265359 ! pi
+  real (KIND=8), parameter :: deg2rad = PI / 180.0 ! convert degrees to radians
+  real (KIND=8), parameter :: pid2 = PI / 2.0 !   pi/2
+  real (KIND=8), parameter :: rad2deg = 180.0 / PI ! convert radians to degrees
+  real (KIND=8), parameter :: twopi = 2 * pi
 
-!  
-!***********************************************************************
+  !  
+  !***********************************************************************
 
-       End Module mathcons
+End Module mathcons
