504cbf9a416265d8caaae40a09dbd794fbe8b687
John C. Houck <jhouck@cfa.harvard.edu>
2016-07-15 18:42:06 -0400

    L2_regrid: regression test script used gcc-specific path to L2_regrid

--- L2_regrid/tests/test_run.sh
+++ L2_regrid/tests/test_run.sh
@@ -2,6 +2,7 @@
 
 # Test data and L2_regrid output goes into this directory
 TMPDIR="tmp"
+L2_REGRID="$1"
 
 # Note that test_run.cfg should be consistent with these values
 NUM_STEPS=40
@@ -9,4 +10,4 @@ NUM_PIXELS=100
 
 mkdir -p $TMPDIR
 ./mktestdata_l2 -o $TMPDIR -m $NUM_STEPS -p $NUM_PIXELS
-../src/gnuobjs/L2_regrid test_run.cfg
+$L2_REGRID test_run.cfg
