4e2c44ec138c281a36776bbfce28984fb7caee71
John C. Houck <jhouck@cfa.harvard.edu>
2017-09-04 08:02:47 -0400

    L0_format: update IRU file format
      * add average_sample_frequency_hz attribute to IRU time variable
      * add gyro bias_time dimension
      * where defined, use file variable, dimension name symbols from libtio

--- L0_format/src/iru.c
+++ L0_format/src/iru.c
@@ -5,6 +5,7 @@
  */
 
 #include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -14,8 +15,9 @@
 
 #include <ioclib.h>
 #include <iocsdpc.h>
-#include <tio.h>
 #include <tell.h>
+#include <tio.h>
+#include <tio_template.h>
 
 #define PROCESS_METHOD_PRIVATE_DATA \
    const char *out_dirname; \
@@ -30,17 +32,14 @@
    unsigned int bias_dimension; \
    void *outbuf; \
    size_t outbuf_num_bytes; \
-   int num_written;
+   int num_written; \
+   int num_files;
 #include "l0_format.h"
 
 #define IRU_FILE_VAR_TIME         "time"
-#define IRU_FILE_VAR_GYRO_OUTPUT  "gyro_output"
 #define IRU_FILE_VAR_SCALE_FACTOR "scale_factor"
 #define IRU_FILE_VAR_BIAS         "bias"
 #define IRU_FILE_VAR_BIAS_TIME    "time_of_scale_or_bias_update"
-
-#define IRU_FILE_DIM_GYRO_AXIS    "gyro_axis"
-#define IRU_FILE_DIM_GYRO_BIAS    "gyro_bias"
 
 #define IRU_BIAS_DIMLEN   3
 
@@ -59,7 +58,7 @@ IRU_Var_Type;
 static IRU_Var_Type IRU_Var_Table[] =
 {
    IRU_VAR(IRU_FILE_VAR_TIME,1,NC_DOUBLE,"sec","Time of sample (secs since TEMPO epoch)"),
-   IRU_VAR(IRU_FILE_VAR_GYRO_OUTPUT,2,NC_INT,"DN","Gyro output data values"),
+   IRU_VAR(TEMPO_VAR_GYRO_OUTPUT,2,NC_INT,"DN","Gyro output data values"),
    IRU_VARS_END
 };
 
@@ -74,6 +73,8 @@ static int close_iru_outfile (Process_Method_Type *pmt)
           return -1;
      }
    pmt->ncid = INT_MAX;
+   pmt->num_written = 0;
+   pmt->num_files = 0;
    return 0;
 }
 
@@ -93,8 +94,8 @@ static int define_iru_vars (Process_Method_Type *pmt,
    IRU_Var_Type *vt;
    int storage = NC_CHUNKED;
    size_t chunk_sizes[2];
-   int start, count, varid;
-   int dimid_time, dimid_gyro_axis, dimid_gyro_bias, dimids[2];
+   int varid, dimid_time, dimid_bias_time, dimid_gyro_axis, dimid_gyro_bias;
+   int dimids_gyro_bias[2], dimids_gyro_output[2], dimids_gyro_scale[2];
    const char *descr_bias_time =
      "Time when gyroscope scale or bias was updated (secs since TEMPO epoch)";
 
@@ -105,59 +106,63 @@ static int define_iru_vars (Process_Method_Type *pmt,
                                          pmt->outfile_timestamp_start))
      return -1;
 
+   /* FIXME?:
+    * The dimension dimid_bias_time is being used somewhat inconsistently.
+    * Each iru0 file incoming from the IOC has a single value of
+    *     time_of_scale_or_bias_update
+    *     scale_factor[gyro_axis]
+    *     bias[gyro_bias]
+    * Instead, the instrument will provide a bias time series, which
+    * the IOC may ultimately provide as a separate file.  For now,
+    * the bias_time dimension is effectively a counter indicating the
+    * number of IRU products from the IOC merged to make the iru0 file.
+    * This configuration is likely to change when we complete NDAs
+    * with the gyroscope vendors and finally learn what's in the IRU
+    * packets.
+    */
+
    /* dimensions */
    if (-1 == TIO_def_dim (pmt->ncid, IRU_FILE_VAR_TIME, NC_UNLIMITED, &dimid_time))
      return -1;
-   if (-1 == TIO_def_dim (pmt->ncid, IRU_FILE_DIM_GYRO_AXIS, pmt->gyro_dimension, &dimid_gyro_axis))
+   if (-1 == TIO_def_dim (pmt->ncid, TEMPO_VAR_TIME_GYRO_BIAS, NC_UNLIMITED, &dimid_bias_time))
      return -1;
-   if (-1 == TIO_def_dim (pmt->ncid, IRU_FILE_DIM_GYRO_BIAS, pmt->bias_dimension, &dimid_gyro_bias))
+   if (-1 == TIO_def_dim (pmt->ncid, TEMPO_DIM_GYRO_AXIS, pmt->gyro_dimension, &dimid_gyro_axis))
      return -1;
-
-   start = 0;
+   if (-1 == TIO_def_dim (pmt->ncid, TEMPO_VAR_GYRO_BIAS, pmt->bias_dimension, &dimid_gyro_bias))
+     return -1;
 
    /* bias time */
-   if (0 != TIO_def_var (pmt->ncid, IRU_FILE_VAR_BIAS_TIME, NC_DOUBLE, 0, NULL, &varid))
+   if (0 != TIO_def_var (pmt->ncid, IRU_FILE_VAR_BIAS_TIME, NC_DOUBLE, 1, &dimid_bias_time, &varid))
      return -1;
    if (0 != annotate_var (pmt->ncid, varid, descr_bias_time, "sec"))
      return -1;
 
-   count = 1;
-   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_BIAS_TIME, &start, &count,
-                                 NC_DOUBLE, &iru->gyro_bias_time))
-     return -1;
-
    /* scale factor */
-   if (0 != TIO_def_var (pmt->ncid, IRU_FILE_VAR_SCALE_FACTOR, NC_FLOAT, 1, &dimid_gyro_axis, &varid))
+   dimids_gyro_scale[0] = dimid_bias_time;
+   dimids_gyro_scale[1] = dimid_gyro_axis;
+   if (0 != TIO_def_var (pmt->ncid, IRU_FILE_VAR_SCALE_FACTOR, NC_FLOAT, 2, dimids_gyro_scale, &varid))
      return -1;
    if (0 != annotate_var (pmt->ncid, varid, "gyro scale factor", NULL))
      return -1;
 
-   count = pmt->gyro_dimension;
-   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_SCALE_FACTOR, &start, &count,
-                                 NC_FLOAT, &iru->gyro_scale_factor))
-     return -1;
-
-   /* bias */
-   if (0 != TIO_def_var (pmt->ncid, IRU_FILE_VAR_BIAS, NC_FLOAT, 1, &dimid_gyro_bias, &varid))
+   /* gyro bias */
+   dimids_gyro_bias[0] = dimid_bias_time;
+   dimids_gyro_bias[1] = dimid_gyro_bias;
+   if (0 != TIO_def_var (pmt->ncid, IRU_FILE_VAR_BIAS, NC_FLOAT, 2, dimids_gyro_bias, &varid))
      return -1;
    if (0 != annotate_var (pmt->ncid, varid, "gyro bias", "microrad/sec"))
      return -1;
 
-   count = pmt->bias_dimension;
-   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_BIAS, &start, &count,
-                                 NC_FLOAT, &iru->gyro_bias))
-     return -1;
-
    /* gyro output time series */
-   dimids[0] = dimid_time;
-   dimids[1] = dimid_gyro_axis;
+   dimids_gyro_output[0] = dimid_time;
+   dimids_gyro_output[1] = dimid_gyro_axis;
 
    chunk_sizes[0] = 1024;
    chunk_sizes[1] = pmt->gyro_dimension;
 
    for (vt = IRU_Var_Table; vt->varname != NULL; vt++)
      {
-        if (0 != TIO_def_var (pmt->ncid, vt->varname, vt->type, vt->ndims, dimids, &varid))
+        if (0 != TIO_def_var (pmt->ncid, vt->varname, vt->type, vt->ndims, dimids_gyro_output, &varid))
           return -1;
         if (0 != TIO_def_var_chunking (pmt->ncid, varid, storage, chunk_sizes))
           return -1;
@@ -194,13 +199,50 @@ static int realloc_outbuf (Process_Method_Type *pmt, size_t num_bytes)
    return 0;
 }
 
+static int write_avg_sample_freq_attr (Process_Method_Type *pmt,
+                                       double *sample_time,
+                                       unsigned int num_records)
+{
+   TIO_Var_Info_Type info;
+   double t0, t1, average_sample_frequency_hz;
+   double delta_sum = 0.0;
+   unsigned int i, num_deltas = 0;
+
+   if (0 != TIO_inq_var (pmt->ncid, IRU_FILE_VAR_TIME, &info))
+     return -1;
+
+   t0 = sample_time[0];
+   for (i = 1; i < num_records; i++)
+     {
+        t1 = sample_time[i];
+        if ((0 == isnan(t0)) && (0 == isnan(t1)))
+          {
+             delta_sum += (t1 - t0);
+             num_deltas += 1;
+          }
+        t0 = t1;
+     }
+   if (delta_sum > 0.0)
+     {
+        average_sample_frequency_hz = num_deltas / delta_sum;
+     }
+   else average_sample_frequency_hz = 0.0;
+
+   if (0 != TIO_put_att (pmt->ncid, info.varid, "average_sample_frequency_hz",
+                         NC_DOUBLE, 1, &average_sample_frequency_hz))
+     return -1;
+
+   return 0;
+}
+
 static int write_iru_records (Process_Method_Type *pmt,
-                              unsigned int num_records,
+                              IOCSDPC_IRU_Type *iru,
                               const IOCSDPC_IRU_Record_Type *rec_array)
 {
+   unsigned int num_records = iru->num_records;
    double time_last_sample = rec_array[num_records-1].sample_time;
    double *sample_time;
-   int start[2], count[2];
+   int start[2], count[2], start_per_file[2], count_per_file[2];
    unsigned int i, ng;
    int32_t *gyro_data;
    size_t num_bytes;
@@ -223,6 +265,9 @@ static int write_iru_records (Process_Method_Type *pmt,
         sample_time[i] = rec_array[i].sample_time;
      }
 
+   if (0 != write_avg_sample_freq_attr (pmt, sample_time, num_records))
+     return -1;
+
    if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_TIME, start, count, NC_DOUBLE, sample_time))
      return -1;
 
@@ -233,10 +278,31 @@ static int write_iru_records (Process_Method_Type *pmt,
         memcpy ((char *)&gyro_data[ng*i], (char *)rec_array[i].gyro_data, ng*sizeof(int32_t));
      }
 
-   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_GYRO_OUTPUT, start, count, NC_INT, gyro_data))
+   if (0 != TIO_put_var_section (pmt->ncid, TEMPO_VAR_GYRO_OUTPUT, start, count, NC_INT, gyro_data))
      return -1;
 
    pmt->num_written += num_records;
+
+   start_per_file[0] = pmt->num_files;
+   start_per_file[1] = 0;
+   count_per_file[0] = 1;
+
+   count_per_file[1] = pmt->bias_dimension;
+   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_BIAS_TIME, start_per_file, count_per_file,
+                                 NC_DOUBLE, &iru->gyro_bias_time))
+     return -1;
+
+   count_per_file[1] = pmt->bias_dimension;
+   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_BIAS, start_per_file, count_per_file,
+                                 NC_FLOAT, &iru->gyro_bias))
+     return -1;
+
+   count_per_file[1] = pmt->gyro_dimension;
+   if (0 != TIO_put_var_section (pmt->ncid, IRU_FILE_VAR_SCALE_FACTOR, start_per_file, count_per_file,
+                                 NC_FLOAT, &iru->gyro_scale_factor))
+     return -1;
+
+   pmt->num_files += 1;
 
    return 0;
 }
@@ -315,7 +381,7 @@ static int process_iru (Process_Method_Type *pmt, const TPInfo_Type *tpinfo,
    if (0 != select_iru_outfile (pmt, iru, rec_array[0].sample_time))
      goto return_status;
 
-   if (0 != write_iru_records (pmt, iru->num_records, rec_array))
+   if (0 != write_iru_records (pmt, iru, rec_array))
      goto return_status;
 
    iocsdpc_iru_close (iru);
