0d5f65ac6a0e6920475bc402374714b5cebda421
John C. Houck <jhouck@cfa.harvard.edu>
2017-10-21 07:29:06 -0400

    libtio: mkgranule_ident writes out tempo epoch TAI start/end timestamps

--- libtio/src/mkgranule_ident.c
+++ libtio/src/mkgranule_ident.c
@@ -50,6 +50,8 @@ static int write_granule_ident_file (const char *file,
    fprintf (fp, "scan_seq_num,%d\n", gid->scan_seq_num);
    fprintf (fp, "granule_seq_num,%d\n", gid->granule_seq_num);
    fprintf (fp, "granule_num,%d\n", gid->granule_num);
+   fprintf (fp, "time_coverage_start_since_epoch,%0.16e\n", gid->tstart);
+   fprintf (fp, "time_coverage_end_since_epoch,%0.16e\n", gid->tend);
 
    fprintf (fp, "tstart_year,%d\n", tstart.tm_year + 1900);
    fprintf (fp, "tstart_month,%d\n", tstart.tm_mon + 1);
