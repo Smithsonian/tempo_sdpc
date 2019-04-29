88b86b852390d0172252e65cfa96f3e946d669e0
John C. Houck <jhouck@cfa.harvard.edu>
2019-04-29 09:17:43 -0400

    pipe: add register.py script to update a sqlite database of product files

---
+++ pipe/src/register.py
@@ -0,0 +1,164 @@
+#! /usr/bin/python
+
+import os, sys
+import sqlite3
+from datetime import date
+from netCDF4 import Dataset as NetCDFFile
+
+Radiance_Table_Name = "rad"
+
+class Table_Type:
+
+    def __init__ (self, table_name, fields, quals):
+        self.table_name = table_name
+        self.field_defs = fields
+        field_list = ','.join('{} {}'.format (k,fields[k]) for k in fields.keys())
+        self.create_cmd = "CREATE TABLE IF NOT EXISTS {table_name} ({field_list}, {quals});".format (**locals())
+
+    def create(self, cur):
+        """Create an empty table"""
+        cur.execute (self.create_cmd)
+
+    def new_entry(self, cur, names, values):
+        """Insert a table entry"""
+        self.create(cur)
+        name_list=','.join(names)
+        value_slots = ','.join(['?']*len(values))
+        cmd = "INSERT INTO {} ({}) VALUES ({})".format(self.table_name, name_list, value_slots)
+        value_string_tuple = tuple(str(v) for v in values)
+        cur.execute (cmd, value_string_tuple)
+
+def init_radiance_table ():
+    fields = {}
+    fields["istart"] = "integer not null"
+    fields["scan_num"] = "integer not null"
+    fields["granule_num"] = "integer not null"
+    fields["time_coverage_start_since_epoch"] = "float not null"
+    fields["time_coverage_end_since_epoch"] = "float not null"
+    fields["filename"] = "text"
+    #fields["mirror_pos_beg"] = "integer not null"
+    #fields["mirror_pos_end"] = "integer not null"
+    quals = "primary key(istart), unique(istart)"
+    return Table_Type(Radiance_Table_Name, fields, quals)
+
+def init_product_table (table_name):
+    fields = {}
+    fields["istart"] = "integer not null"
+    fields["mtime"] = "integer"
+    fields["size"] = "integer"
+    fields["filename"] = "text"
+    quals = "unique(istart), foreign key (istart) references {}(istart)".format(Radiance_Table_Name)
+    return Table_Type(table_name, fields, quals)
+
+def insert_radiance_entry (conn, entry):
+    with conn:
+        c = conn.cursor()
+        rad = init_radiance_table()
+        rad.create(c)
+        try:
+            rad.new_entry (c, entry.keys(), entry.values())
+            conn.commit()
+            return 0
+        except sqlite3.IntegrityError:
+            print ('ERROR: duplicate primary key: istart={}'.format(entry["istart"]))
+            return -1
+
+def insert_product_entry (conn, product_name, entry):
+    with conn:
+        c = conn.cursor()
+        p = init_product_table (product_name)
+        p.create(c)
+        keys = p.field_defs.keys()
+        key_values = [entry[k] for k in keys]
+        try:
+            p.new_entry (c, keys, key_values)
+            conn.commit()
+            return 0
+        except sqlite3.IntegrityError:
+            print ('ERROR: duplicate primary key: istart={}'.format(entry["istart"]))
+            return -1
+
+def get_radiance_keys (keys, attr):
+    want_keys = ["time_coverage_start_since_epoch", "time_coverage_end_since_epoch",
+                 "scan_num", "granule_num"]
+    for k in want_keys:
+        if k in attr:
+            keys[k] = attr[k]
+    return keys
+
+def process_file (conn, filename):
+
+    basename = os.path.basename (filename)
+    tok = basename.split('_')
+
+    dont_process = ["tie"]
+    if (tok[1] in dont_process):
+        return 0
+
+    product_name = '{}_{}'.format(tok[1], tok[2])
+
+    nc = NetCDFFile(filename, "r")
+    attr = nc.__dict__
+    keys = {}
+
+    k = "time_coverage_start_since_epoch"
+    if not k in attr:
+        print ("WARNING: {} not in file {}".format (k, filename))
+        return -1
+
+    keys["istart"] = int(attr[k])
+    keys["filename"] = basename
+
+    # FIXME: Eventually, rad_L0 will define the primary radiance granule
+    # and all other files derived from it will be treated as products,
+    # recording only istart, mtime, size
+
+    if (tok[1] == 'rad'):
+        get_radiance_keys (keys, attr)
+        status = insert_radiance_entry (conn, keys)
+        if status < 0:
+           print('ERROR: processing file {}'.format(filename))
+
+    st = os.stat(filename)
+    keys["mtime"] = st.st_mtime
+    keys["size"] = st.st_size
+
+    status = insert_product_entry (conn, product_name, keys)
+    if status < 0:
+        print('ERROR: processing file {}'.format(filename))
+
+    return status
+
+def make_db_path ():
+    db_dir = os.getenv("SDPC_ARCHIVE_DIR", ".")
+    db_basename = date.today().strftime("production_%Y%m.db")
+    if db_dir != ".":
+        db_dir = os.path.join (db_dir, "stat")
+    if not os.path.isdir(db_dir):
+        os.makedirs(db_dir)
+    return os.path.join (db_dir, db_basename)
+
+def main():
+
+    if len(sys.argv) == 1:
+        print('Usage:  {} FILE ...'.format(sys.argv[0]))
+        sys.exit(0)
+
+    filenames = sys.argv[1:]
+
+    db_path = make_db_path()
+    conn = sqlite3.connect (db_path)
+
+    status = 0
+
+    for fn in filenames:
+        if os.path.isfile(fn):
+            status += process_file (conn, fn)
+        else:
+            print('WARNING: nonexistent file: {}'.format(fn))
+
+    conn.close()
+    sys.exit(status)
+
+if __name__ == "__main__":
+    main()
