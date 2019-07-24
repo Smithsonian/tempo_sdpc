d667831d555de0c475fafa910dbb4210953ab71c
John C. Houck <jhouck@cfa.harvard.edu>
2019-07-24 14:53:59 -0400

    pipe: increase verbosity in L0_ccd log

--- pipe/slurm/run_wavecal_merge.sh
+++ pipe/slurm/run_wavecal_merge.sh
@@ -15,4 +15,9 @@ case "$bn" in
     ;;
 esac
 
+### If the verbose option of wavecal_driver is turned on, you
+### may want to uncomment these two lines to collect the log files:
+# wavecal_log_files=$(ls $result_dir/*.log)
+# tar czv --remove-files -f wavecal_logs.tar.gz $wavecal_log_files
+
 wavecal_merge $args --delete -t $input_file $result_dir
