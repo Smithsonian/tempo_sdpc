a240a40745b781f80716ad79488bdfd4f5ee3167
John C. Houck <jhouck@cfa.harvard.edu>
2019-07-28 04:42:36 -0400

    L1_trace_gas: increase max granule dimensions for TEMPO

--- L1_trace_gas/src/omi_parameters_module.f90
+++ L1_trace_gas/src/omi_parameters_module.f90
@@ -6,7 +6,7 @@ MODULE omi_parameters_module
   ! ---------------------------------
   INTEGER (KIND=i4), PARAMETER :: &
     omi_nxtrack_max    = 2048, & !60, &   ! JCH 2048 is required for TEMPO
-    omi_nwavel_max     = 1026, &  ! JCH synthetic data uses 1026
+    omi_nwavel_max     = 1028, &
     omi_nwavelcoef_max =   5
 
 END MODULE
