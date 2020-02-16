3c2ef86bf06e15ef4641822abea6dd7bc2b223f0
John C. Houck <jhouck@cfa.harvard.edu>
2020-02-16 05:30:13 -0500

    libtio: added level1_info to facilitate restructuring slurm pipeline

---
+++ libtio/src/level1_info.c
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define __USE_XOPEN
+#include <time.h>
+
+#include <limits.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <getopt.h>
+
+#include <netcdf.h>
+#include <tell.h>
+
+#include "tio.h"
+#include "tio_template.h"
+#include "_tio.h"
+
+#define MAX_PRODUCT_TYPE_LEN 16
+
+enum
+{
+   TASK_UNKNOWN,
+   TASK_PRINT_DIR,
+   TASK_PRINT_MONTH
+};
+
+static void usage (int argc, char **argv)
+{
+   (void) argc;
+   fprintf (stderr, "Usage: %s <option> <level1-product-file>\n", argv[0]);
+   fprintf (stderr, "Options:\n");
+   fprintf (stderr, "  -d | --dir    Print the archive sub-directory path for this file\n");
+   fprintf (stderr, "  -m | --month  Print the calendar month when this file started\n");
+}
+
+static int print_archive_subdir (const _pTIO_Granule_Ident_Type *gid, const char *product_type)
+{
+   double flocal_tstart;
+
+   if (0 != tio_time_sat_local_day_number (gid->tstart, &flocal_tstart))
+     {
+        tell_verror (TELL_RUNTIME_ERROR, "%s: computing satellite-local day number", __func__);
+        return -1;
+     }
+
+   /* The archive is organized by major Level 1 product types:
+    *    DRK, DRKL
+    *    IRR, IRRL
+    *    RAD, RADT
+    * "DRK" and "IRR" products are organized by day/tstart.
+    * "RAD" products are organized by day/scan/granule index.
+    */
+
+   if (gid->granule_num > 0)
+     {
+        /* All Level 1 radiance file variants should have RAD in the product_type.
+         * If that's missing, then this file type is not supported.
+         */
+        if (NULL == strstr (product_type, "RAD"))
+          {
+             tell_verror (TELL_RUNTIME_ERROR, "%s: unsupported product type: %s", __func__, product_type);
+             return -1;
+          }
+        if (fprintf (stdout, "%s/D%05d/S%03d/G%02d", product_type,
+                     (int) flocal_tstart, gid->scan_num, gid->granule_num) < 0)
+          return -1;
+     }
+   else  /* All other Level1 file types are organized by product_type/date/tstart */
+     {
+        char buf[MAX_ISOTIME_LEN];
+
+        if (0 != TIO_mktimestamp_str (gid->tstart, 0, buf, sizeof(buf)))
+          return -1;
+
+        if (fprintf (stdout, "%s/D%05d/%s", product_type,
+                     (int) flocal_tstart, buf) < 0)
+          return -1;
+     }
+
+   return 0;
+}
+
+static int print_product_month (const _pTIO_Granule_Ident_Type *gid)
+{
+   struct tm tstart;
+   const char *month[] = {
+      "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+      "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"
+   };
+
+   if ((-1 == _pTIO_parse_timestr (gid->tstart_str, &tstart))
+       || (tstart.tm_mon < 0 || tstart.tm_mon > 11))
+     return -1;
+
+   if (fprintf (stdout, "%s", month[tstart.tm_mon]) < 0)
+     return -1;
+
+   return 0;
+}
+
+int main (int argc, char **argv)
+{
+   _pTIO_Granule_Ident_Type gid = {0};
+   char product_type[MAX_PRODUCT_TYPE_LEN];
+   char *level1_file = NULL;
+   int task = TASK_UNKNOWN;
+   int status = 1;
+   int ncid, xtype;
+   size_t len;
+   static struct option long_options[] =
+     {
+        {"dir",   no_argument, 0, 'd'},
+        {"month", no_argument, 0, 'm'},
+	{"help",  no_argument, 0, 'h'},
+        {0,0,0,0}
+     };
+
+   for (;;)
+     {
+        int option_index = 0;
+        int c = getopt_long (argc, argv, "dm", long_options, &option_index);
+        if (c == -1) break;
+
+        switch (c)
+          {
+           case 'd':
+             task = TASK_PRINT_DIR;
+             break;
+
+           case 'm':
+             task = TASK_PRINT_MONTH;
+             break;
+
+           case '?':
+             fprintf (stderr, "Unknown option -%c'.\n", optopt);
+             usage (argc, argv);
+             return 1;
+
+           default:
+             usage (argc, argv);
+             return 1;
+          }
+     }
+
+   if (argc - optind < 1)
+     {
+        usage (argc, argv);
+        return 1;
+     }
+
+   if (task == TASK_UNKNOWN)
+     {
+        usage (argc, argv);
+        return 1;
+     }
+
+   level1_file = argv[optind];
+
+   if (0 != TIO_open (level1_file, NC_NOWRITE, &ncid))
+     return 1;
+
+   if ((0 != tio_use_file_epoch (ncid))
+       || (-1 == _pTIO_read_granule_ident (ncid, &gid)))
+     {
+        (void) TIO_close (ncid);
+        return 1;
+     }
+
+   memset (product_type, 0, MAX_PRODUCT_TYPE_LEN);
+
+   if ((NC_NOERR == nc_inq_att (ncid, NC_GLOBAL, "product_type", &xtype, &len))
+       && (len < MAX_PRODUCT_TYPE_LEN))
+     {
+        if (0 != TIO_get_att (ncid, NC_GLOBAL, "product_type", NC_CHAR, product_type))
+          {
+             (void) TIO_close (ncid);
+             return 1;
+          }
+     }
+
+   (void) TIO_close (ncid);
+
+   switch (task)
+     {
+      case TASK_PRINT_DIR:
+        status = print_archive_subdir (&gid, product_type);
+        break;
+
+      case TASK_PRINT_MONTH:
+        status = print_product_month (&gid);
+        break;
+
+      default:
+        tell_verror (TELL_RUNTIME_ERROR, "%s: invalid task", __func__);
+        break;
+     }
+
+   return status ? EXIT_FAILURE : EXIT_SUCCESS;
+}
