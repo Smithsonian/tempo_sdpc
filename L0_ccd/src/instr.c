b3de4c2decb0412477270c43d3bec23913b85ea6
John C. Houck <jhouck@cfa.harvard.edu>
2020-08-09 16:11:43 -0400

    L0_ccd: telemetry points in different sections may have different sample times

--- L0_ccd/src/instr.c
+++ L0_ccd/src/instr.c
@@ -15,12 +15,19 @@
 #include "config.h"
 #include "util.h"
 
-#define INSTR_PRIVATE_DATA \
-   Instr_Type *next; \
-   double *timestamp; \
-   float *adc_temp0_derived; \
-   float *fpe_temp1; \
+typedef struct TP_Type TP_Type;
+
+struct TP_Type
+{
+   TP_Type *next;
+   double *timestamp;
+   float *value;
    size_t num_times;
+};
+
+#define INSTR_PRIVATE_DATA \
+   TP_Type *adc_temp0_derived; \
+   TP_Type *fpe_temp1;
 #include "instr.h"
 
 typedef struct
@@ -31,10 +38,10 @@ typedef struct
 }
 Instr_Filter_Type;
 
-static int find_entry1 (const Instr_Type *instr, double t, int *entry)
+static int find_entry1 (const TP_Type *tp, double t, int *entry)
 {
-   double *ti = instr->timestamp;
-   int n = instr->num_times;
+   double *ti = tp->timestamp;
+   int n = tp->num_times;
 
    if (ti[0] <= t && t <= ti[n-1])
      {
@@ -53,30 +60,30 @@ static int find_entry1 (const Instr_Type *instr, double t, int *entry)
      }
 }
 
-static const Instr_Type *find_entry (const Instr_Type *instr, double t,
-                                     int *entry, int *entry_status)
+static const TP_Type *find_entry (const TP_Type *tp, double t,
+                                  int *entry, int *entry_status)
 {
-   const Instr_Type *closest_instr = NULL;
+   const TP_Type *closest_tp = NULL;
    int closest_status = -1;
    int closest_index = 0;
    double min_delta = DBL_MAX;
 
    /* We could merge the lists, but this is probably fast enough */
 
-   for ( ; instr != NULL; instr = instr->next)
+   for ( ; tp != NULL; tp = tp->next)
      {
         int index, status;
-        status = find_entry1 (instr, t, &index);
+        status = find_entry1 (tp, t, &index);
         if (status == 0)
           {
              *entry = index;
              *entry_status = 0;
-             return instr;
+             return tp;
           }
-        else if (fabs(t-instr->timestamp[index]) < min_delta)
+        else if (fabs(t-tp->timestamp[index]) < min_delta)
           {
              closest_index = index;
-             closest_instr = instr;
+             closest_tp = tp;
              closest_status = status;
           }
      }
@@ -84,46 +91,115 @@ static const Instr_Type *find_entry (const Instr_Type *instr, double t,
    *entry_status = closest_status;
    *entry = closest_index;
 
-   return closest_instr;
+   return closest_tp;
 }
 
+/* FIXME: should get FPE and FPA temps separately, so the status can be returned separately for each */
 static int instr_temps (const Instr_Type *instr, double timestamp,
                         float *fpa_temp, float *fpe_temp)
 {
-   const Instr_Type *this_instr = NULL;
+   const TP_Type *tp = NULL;
    int index, index_status;
-   this_instr = find_entry (instr, timestamp, &index, &index_status);
-   if (this_instr)
+
+   if (NULL == (tp = find_entry (instr->adc_temp0_derived, timestamp, &index, &index_status)))
      {
-        *fpa_temp = this_instr->adc_temp0_derived[index];
-        *fpe_temp = this_instr->fpe_temp1[index];
-        return index_status;
+        tell_verror (TELL_UNKNOWN_ERROR, "%s: FPA temperature lookup failed", __func__);
+        return -2;
      }
-   tell_verror (TELL_UNKNOWN_ERROR, "%s: temperature lookup failed", __func__);
-   return -2;
+   *fpa_temp = tp->value[index];
+   tell_vlog (TELL_MSGTYPE_INFO, 2, "FPA temp lookup: time: %0.3f => FPA temp: %0.2f C", timestamp, *fpa_temp);
+
+   if (NULL == (tp = find_entry (instr->fpe_temp1, timestamp, &index, &index_status)))
+     {
+        tell_verror (TELL_UNKNOWN_ERROR, "%s: FPE temperature lookup failed", __func__);
+        return -2;
+     }
+   *fpe_temp = tp->value[index];
+   tell_vlog (TELL_MSGTYPE_INFO, 2, "FPE temp lookup: time: %0.3f => FPE temp: %.2f C", timestamp, *fpe_temp);
+
+   return 0;
 }
 
-static void free_instr1 (Instr_Type *instr)
+static void free_tp1 (TP_Type *tp)
 {
-   if (instr == NULL)
+   if (tp == NULL)
      return;
-   FREE(instr->timestamp);
-   FREE(instr->adc_temp0_derived);
-   FREE(instr->fpe_temp1);
-   FREE(instr);
+   FREE(tp->timestamp);
+   FREE(tp->value);
+   FREE(tp);
+}
+
+static void free_tp (TP_Type *tp)
+{
+   while (tp)
+     {
+        TP_Type *next = tp->next;
+        free_tp1(tp);
+        tp = next;
+     }
+}
+
+static TP_Type *alloc_tp (size_t num_times)
+{
+   TP_Type *tp = NULL;
+
+   if (NULL == (tp = (TP_Type *)MALLOC (sizeof *tp)))
+     {
+        tell_verror (TELL_MALLOC_ERROR, "%s: malloc failed", __func__);
+        return NULL;
+     }
+
+   if ((NULL == (tp->timestamp = (double *) MALLOC (num_times * sizeof(double))))
+       || (NULL == (tp->value = (float *) MALLOC (num_times * sizeof(float)))))
+     {
+        tell_verror (TELL_MALLOC_ERROR, "%s: malloc failed", __func__);
+        free_tp1 (tp);
+        return NULL;
+     }
+
+   tp->num_times = num_times;
+   tp->next = NULL;
+
+   return tp;
+}
+
+static int append_tp (TP_Type **head, TP_Type *tp)
+{
+   TP_Type *p;
+
+   if (tp == NULL)
+     return -1;
+
+   if (head == NULL)
+     {
+        tell_verror (TELL_RUNTIME_ERROR, "%s: head pointer is NULL (should never happen!)", __func__);
+        return -1;
+     }
+
+   if (*head == NULL)
+     {
+        *head = tp;
+        return 0;
+     }
+
+   for (p = *head; p->next != NULL; p = p->next)
+     {}
+
+   p->next = tp;
+
+   return 0;
 }
 
 static void free_instr (Instr_Type *instr)
 {
-   while (instr)
-     {
-        Instr_Type *next = instr->next;
-        free_instr1 (instr);
-        instr = next;
-     }
+   if (instr == NULL)
+     return;
+   free_tp (instr->adc_temp0_derived);
+   free_tp (instr->fpe_temp1);
+   FREE(instr);
 }
 
-static Instr_Type *new_instr_type (size_t num_times)
+static Instr_Type *new_instr_type (void)
 {
    Instr_Type *instr = NULL;
 
@@ -134,16 +210,6 @@ static Instr_Type *new_instr_type (size_t num_times)
      }
    memset ((char *)instr, 0, sizeof *instr);
 
-   if ((NULL == (instr->timestamp = (double *) MALLOC (num_times * sizeof(double))))
-       || (NULL == (instr->adc_temp0_derived = (float *) MALLOC (num_times * sizeof(float))))
-       || (NULL == (instr->fpe_temp1 = (float *) MALLOC (num_times * sizeof(float)))))
-     {
-        tell_verror (TELL_MALLOC_ERROR, "%s: malloc failed", __func__);
-        free_instr1 (instr);
-        return NULL;
-     }
-
-   instr->num_times = num_times;
    instr->instr_delete = free_instr;
    instr->instr_temps = instr_temps;
 
@@ -161,12 +227,12 @@ static int Qsort_Index_Compare (const void *a, const void *b)
    else return 0;
 }
 
-static int time_sort1 (Instr_Type *instr)
+static int time_sort1 (TP_Type *tp)
 {
    int *sort_index = NULL;
-   size_t i, num_keys = instr->num_times;
+   size_t i, num_keys = tp->num_times;
 
-   if (NULL == (sort_index = (int *) MALLOC (instr->num_times * sizeof(int))))
+   if (NULL == (sort_index = (int *) MALLOC (tp->num_times * sizeof(int))))
      {
         tell_verror (TELL_MALLOC_ERROR, "%s: malloc failed", __func__);
         return -1;
@@ -175,9 +241,9 @@ static int time_sort1 (Instr_Type *instr)
    for (i = 0; i < num_keys; i++)
      sort_index[i] = i;
 
-   Qsort_Keys = instr->timestamp;
+   Qsort_Keys = tp->timestamp;
    Qsort_Indices = sort_index;
-   num_keys = instr->num_times;
+   num_keys = tp->num_times;
    qsort (sort_index, num_keys, sizeof(int), Qsort_Index_Compare);
 
    FREE(sort_index);
@@ -188,28 +254,69 @@ static int time_sort1 (Instr_Type *instr)
 
 static TIO_Meta_Type *_pMeta_Ptr;
 
-static Instr_Type *read_instr1 (const char *file)
+static TP_Type *read_telemetry_point (int ncid, const char *grp_name,
+                                      const char *var_name)
 {
-   Instr_Type *instr = NULL;
-   int ncid, grp, dimid, start, count;
+   TP_Type *tp = NULL;
+   int group_exists, grp, dimid, start, count, varid;
    size_t num_times;
+
+   tell_push_queue();
+   group_exists = (0 == TIO_inq_grp (ncid, grp_name, &grp));
+   tell_pop_queue (1);
+   if (0 == group_exists)
+     return NULL;
+
+   if (NC_NOERR != tio_inq_varid (grp, var_name, &varid))
+     {
+        tell_vwarn (0, "%s: section %s: variable %s not present\n", __func__, grp_name, var_name);
+        return NULL;
+     }
+
+   if (0 != TIO_inq_dim (grp, "time", &dimid, &num_times))
+     return NULL;
+
+   if (NULL == (tp = alloc_tp (num_times)))
+     return NULL;
+
+   start = 0;
+   count = num_times;
+
+   if ((0 != TIO_get_var_section (grp, "time", &start, &count, TIO_DOUBLE, tp->timestamp))
+       || (0 != TIO_get_var_section (grp, var_name, &start, &count, TIO_FLOAT, tp->value)))
+     {
+        tell_verror (TELL_IO_READ_ERROR, "%s: reading instrument telemetry point %s",  __func__, var_name);
+        goto free_and_return;
+     }
+
+   /* FIXME: probably don't need to do this */
+   if (0 != time_sort1 (tp))
+     goto free_and_return;
+
+   return tp;
+free_and_return:
+   free_tp1 (tp);
+   return NULL;
+}
+
+static int read_instr1 (Instr_Type *instr, const char *file)
+{
+   /* FIXME - after fixing test data, stop looking for adc_temp0_derived in fpe_analog_tlm2
+    * Some of the level 0 test data erroneously put adc_temp0_derived in fpe_analog_tlm2
+    * instead of in adc_tlm (mea culpa).  In the near term, it's simplest to have the code
+    * look in adc_tlm first, and then check for fpe_analog_tlm2 as a fallback.
+    * With real data this should not be necessary.  The problem should be corrected in
+    * the next set of test data.  Once this workaround is no longer needed, remove it!
+    */
+   char *adc_temp0_sections[] = {"adc_tlm", "fpe_analog_tlm2", NULL};
+   char **section;
+   int ncid, found_values = 0;
+   TP_Type *tp;
 
    tell_vlog (TELL_MSGTYPE_INFO, 1, "reading %s", file);
 
    if (0 != TIO_open (file, NC_NOWRITE, &ncid))
-     return NULL;
-
-   if (0 != TIO_inq_grp (ncid, "fpe_analog_tlm2", &grp))
-     goto return_error;
-
-   if (0 != TIO_inq_dim (grp, "time", &dimid, &num_times))
-     goto return_error;
-
-   if (NULL == (instr = new_instr_type (num_times)))
-     goto return_error;
-
-   start = 0;
-   count = num_times;
+     return -1;
 
    /* For the FPA temperature, the Command and Telemetry Handbook contains
     * at least 5 telemetry points that are related. Dave Flittner (NASA/LARC)
@@ -227,49 +334,51 @@ static Instr_Type *read_instr1 (const char *file)
     * in Celsius so we use that directly.
     */
 
-   if ((0 != TIO_get_var_section (grp, "time", &start, &count, TIO_DOUBLE,
-                                  instr->timestamp))
-       || (0 != TIO_get_var_section (grp, "adc_temp0_derived", &start, &count, TIO_FLOAT,
-                                     instr->adc_temp0_derived))
-       || (0 != TIO_get_var_section (grp, "fpe_temp1", &start, &count, TIO_FLOAT,
-                                     instr->fpe_temp1))
-      )
+   for (section = adc_temp0_sections; section != NULL; section++)
      {
-        tell_verror (TELL_IO_READ_ERROR, "%s: reading instrument status: %s",
-                     __func__, file);
-        goto return_error;
+        if (NULL != (tp = read_telemetry_point (ncid, *section, "adc_temp0_derived")))
+          {
+             if (0 != append_tp (&instr->adc_temp0_derived, tp))
+               goto return_error;
+             found_values++;
+             break;
+          }
      }
 
-   if (-1 == time_sort1 (instr))
-     goto return_error;
+   if (NULL != (tp = read_telemetry_point (ncid, "fpe_analog_tlm2", "fpe_temp1")))
+     {
+        if (0 != append_tp (&instr->fpe_temp1, tp))
+          goto return_error;
+        found_values++;
+     }
 
-   if (0 != meta_record_basename (_pMeta_Ptr, file))
-     goto return_error;
+   if (found_values)
+     {
+        if (0 != meta_record_basename (_pMeta_Ptr, file))
+          goto return_error;
+     }
 
    TIO_close (ncid);
 
-   return instr;
+   return 0;
 
 return_error:
    TIO_close (ncid);
-   free_instr1 (instr);
-   return NULL;
+   return -1;
 }
 
-static Instr_Type *read_instr_list (const char *path)
+static int read_instr_list (Instr_Type *instr, const char *path)
 {
    FILE *fp = NULL;
-   Instr_Type *head = NULL;
 
    if (NULL == (fp = fopen (path, "r")))
      {
         tell_verror (TELL_IO_OPEN_ERROR, "%s: opening %s", __func__, path);
-        return NULL;
+        return -1;
      }
 
    for (;;)
      {
-        Instr_Type *instr, **tail;
         char *newline;
         char buf[1024];
 
@@ -281,25 +390,13 @@ static Instr_Type *read_instr_list (const char *path)
         if (NULL != (newline = strchr (buf, '\n')))
           *newline = 0;
 
-        if (NULL == (instr = read_instr1 (buf)))
-          {
-             free_instr (instr);
-             break;
-          }
-
-        /* Preserve the order, in case the files are sorted */
-        if (head == NULL)
-          head = instr;
-        else
-          {
-             *tail = instr;
-          }
-        tail = &instr->next;
+        if (0 != read_instr1 (instr, buf))
+          return -1;
      }
 
    fclose (fp);
 
-   return head;
+   return 0;
 }
 
 enum
@@ -358,21 +455,21 @@ static int filter_excludes_file (const Instr_Filter_Type *flt, const char *file,
    return 0;
 }
 
-static Instr_Type *read_instr_glob (Instr_Type *head, const char *path, const Instr_Filter_Type *flt)
+static int read_instr_glob (Instr_Type *instr, const char *path, const Instr_Filter_Type *flt)
 {
    IOCLib_Glob_Type *g = NULL;
-   Instr_Type **tail = NULL;
    char *glob_path = NULL;
    unsigned int i;
    double min_delta_t = DBL_MAX;
    int nearest = -1;
-   int status_flag = -1;
+   int status = -1;
+   int num_files_selected = 0;
 
    if (flt->glob_basename == NULL)
      {
         tell_verror (TELL_INVALID_PARM_ERROR,
                      "%s: glob_basename == NULL", __func__);
-        return NULL;
+        return -1;
      }
 
    if (NULL == (glob_path = ioclib_pathconcat (path, flt->glob_basename)))
@@ -389,7 +486,6 @@ static Instr_Type *read_instr_glob (Instr_Type *head, const char *path, const In
 
    for (i = 0; i < g->num_files; i++)
      {
-        Instr_Type *instr;
         double delta_t;
         int flag;
 
@@ -409,35 +505,28 @@ static Instr_Type *read_instr_glob (Instr_Type *head, const char *path, const In
         else if (flag == FILTER_HALTS_SEARCH)
           break;
 
-        if (NULL == (instr = read_instr1 (g->files[i])))
-          goto return_status;
+        num_files_selected++;
 
-        /* Preserve the order */
-        if (head == NULL)
-          head = instr;
-        else
-          {
-             *tail = instr;
-          }
-        tail = &instr->next;
+        if (0 != read_instr1 (instr, g->files[i]))
+          goto return_status;
      }
 
-   if (head == NULL)
+   if (num_files_selected == 0)
      {
         tell_vwarn (0, "%s: no files match selection criteria: %s",
                     __func__, glob_path);
         if (nearest >= 0)
           {
-             if (NULL == (head = read_instr1 (g->files[nearest])))
+             if (0 != read_instr1 (instr, g->files[nearest]))
                goto return_status;
              tell_vwarn (0, "%s: using nearest match (delta_t=%f sec): %s",
                          __func__, min_delta_t, g->files[nearest]);
           }
      }
 
-   status_flag = 0;
+   status = 0;
 return_status:
-   if (status_flag)
+   if (status)
      {
 	tell_verror (TELL_RUNTIME_ERROR, "%s: failed", __func__);
      }
@@ -445,13 +534,7 @@ return_status:
    ioclib_free (glob_path);
    ioclib_glob_free (g);
 
-   if (status_flag)
-     {
-        free_instr (head);
-        head = NULL;
-     }
-
-   return head;
+   return status;
 }
 
 static char *make_hk_dir_path (int sat_day)
@@ -481,16 +564,16 @@ static char *make_hk_dir_path (int sat_day)
    return expand_string (buf);
 }
 
-static Instr_Type *read_instr_default_paths (const Instr_Filter_Type *flt)
+static int read_instr_default_paths (Instr_Type *instr, const Instr_Filter_Type *flt)
 {
-   Instr_Type *instr = NULL;
    char *dir = NULL;
    double day_beg, day_end;
    int iday_beg, iday_end;
+   int status_beg, status_end;
 
    if ((0 != tio_time_sat_local_day_number (flt->tstart, &day_beg))
        ||(0 != tio_time_sat_local_day_number (flt->tend, &day_end)))
-     return NULL;
+     return -1;
 
    iday_beg = (int) day_beg;
    iday_end = (int) day_end;
@@ -508,14 +591,13 @@ static Instr_Type *read_instr_default_paths (const Instr_Filter_Type *flt)
         tell_verror (TELL_RUNTIME_ERROR,
                      "%s: invalid/unsupported observation interval: tstart=%f, tend=%f",
                      __func__, flt->tstart, flt->tend);
-        return NULL;
+        return -1;
      }
 
    if (NULL == (dir = make_hk_dir_path (iday_beg)))
-     return NULL;
+     return -1;
 
-   /* NULL ok */
-   instr = read_instr_glob (instr, dir, flt);
+   status_beg = read_instr_glob (instr, dir, flt);
    FREE(dir);
 
    if (iday_end > iday_beg)
@@ -523,17 +605,19 @@ static Instr_Type *read_instr_default_paths (const Instr_Filter_Type *flt)
         if (NULL == (dir = make_hk_dir_path (iday_end)))
           {
              free_instr (instr);
-             return NULL;
+             return -1;
           }
-        /* NULL ok */
-        instr = read_instr_glob (instr, dir, flt);
+        status_end = read_instr_glob (instr, dir, flt);
         FREE(dir);
      }
 
-   return instr;
+   if ((status_beg == 0) || (status_end == 0))
+     return 0;
+
+   return -1;
 }
 
-static Instr_Type *read_instr (const char *path, const Instr_Filter_Type *flt)
+static int read_instr (Instr_Type *instr, const char *path, const Instr_Filter_Type *flt)
 {
    struct stat st = {0};
 
@@ -555,28 +639,28 @@ static Instr_Type *read_instr (const char *path, const Instr_Filter_Type *flt)
 
    if (path == NULL)
      {
-        return read_instr_default_paths (flt);
+        return read_instr_default_paths (instr, flt);
      }
 
    if (*path == '@')
      {
         path++;
-        return read_instr_list (path);
+        return read_instr_list (instr, path);
      }
 
    if (stat(path, &st) == -1)
      {
         tell_verror (TELL_RUNTIME_ERROR, "%s: cannot stat %s",
                      __func__, path ? path : "<null>");
-        return NULL;
+        return -1;
      }
 
    if (S_ISDIR(st.st_mode))
      {
-        return read_instr_glob (NULL, path, flt);
+        return read_instr_glob (instr, path, flt);
      }
 
-   return read_instr1 (path);
+   return read_instr1 (instr, path);
 }
 
 Instr_Type *instr_open (const char *file, const char *glob_basename,
@@ -593,12 +677,16 @@ Instr_Type *instr_open (const char *file, const char *glob_basename,
 
    _pMeta_Ptr = meta;
 
-   instr = read_instr (file, &flt);
+   if (NULL == (instr = new_instr_type ()))
+     return NULL;
 
-   if (instr)
+   if (0 != read_instr (instr, file, &flt))
      {
-        tell_vlog (TELL_MSGTYPE_INFO, 1, "%s: succeeded", __func__);
+        free_instr (instr);
+        return NULL;
      }
+
+   tell_vlog (TELL_MSGTYPE_INFO, 1, "%s: succeeded", __func__);
 
    return instr;
 }
