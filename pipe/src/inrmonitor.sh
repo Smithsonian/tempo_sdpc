09638d6c51c6995862240654d645fec41f5d0474
John C. Houck <jhouck@cfa.harvard.edu>
2020-10-28 07:53:13 -0400

    pipe: add inrmonitor.sh

---
+++ pipe/src/inrmonitor.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+if test -z "$SDPC_PIPE_NAME" ; then
+   printf "*** inrmonitor.sh: SDPC_PIPE_NAME not set\n"
+   exit 1
+fi
+
+top=$(dirname $0)/..
+. $top/etc/sdpc_env.sh
+
+export JAVA_HOME="$SDPC_OTS_ROOT/jdk1.8.0_91"
+export PATH="$JAVA_HOME/bin:$PATH"
+
+export TEMPO_INRSW_HOME="$SDPC_INRSW_ROOT"
+export TEMPO_INRSW_GUI_HOME="$SDPC_INR_RUN_DIR"
+export TEMPO_INRSW_CONFIG_DIR="$SDPC_INR_RUN_DIR/config"
+
+options="-Dprism.order=sw"
+
+java $options -jar $SDPC_INRSW_ROOT/gui/GranuleViewerApp.jar \
+                   $TEMPO_INRSW_CONFIG_DIR/TempoPipelineInterfaceSAO.conf
