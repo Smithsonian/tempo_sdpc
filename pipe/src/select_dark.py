458c890f2636e4e863e2eb4435c149f219ba8afe
John C. Houck <jhouck@cfa.harvard.edu>
2021-04-16 08:52:10 -0400

    pipe: add time filter to DRK and HK database query

--- pipe/src/select_dark.py
+++ pipe/src/select_dark.py
@@ -9,6 +9,7 @@ import sqlite3
 from datetime import date
 from netCDF4 import Dataset as NetCDFFile
 import numpy as np
+import argparse
 
 # python3 will provide file= redirection to stderr
 def eprint(*args, **kwargs):
@@ -40,28 +41,42 @@ def get_level0_keys (filename):
     nc.close()
     return keys
 
-def select_matching_dark (c, keys):
-    tstart = keys["time_coverage_start_since_epoch"]
-    exposure_time_per_coadd = keys["mean_exposure_time_per_coadd"]
-    # FIXME - we'll also need to filter this on start time, but that's tricky when the times in the
-    # test files are incorrect...
+def select_matching_dark (c, window_hours, keys):
+    dt_name="mean_exposure_time_per_coadd"
+    t_name="time_coverage_start_since_epoch"
+
+    tx = keys[t_name]
+    dtx = keys[dt_name]
+    t_ok = window_hours * 3600.0   # time offset acceptable for dark measurements
+
+    subst = {'dtx':dtx, 'tx':tx, 'beg':tx-t_ok, 'end':tx+t_ok}
+
     cmd = "select path from \
-    (      select path, min(mean_exposure_time_per_coadd) as dt from 'DRK_L1' where mean_exposure_time_per_coadd >= {} \
-    union select path, max(mean_exposure_time_per_coadd) as dt from 'DRK_L1' where mean_exposure_time_per_coadd < {}) \
-    order by abs({}-dt);".format (exposure_time_per_coadd, exposure_time_per_coadd, exposure_time_per_coadd)
-    c.execute(cmd)
+    (      select path, min({dt_name}) as dt from 'DRK_L1' where {dt_name} >= :dtx and {t_name} > :beg and {t_name} < :end \
+    union  select path, max({dt_name}) as dt from 'DRK_L1' where {dt_name} < :dtx  and {t_name} > :beg and {t_name} < :end) \
+    order by abs(:dtx-dt);".format (**locals())
+
+    c.execute(cmd, subst)
     rows = c.fetchall()
     rows = [r for r in rows if None not in r]
+
     return rows
 
 def main():
-
-    if len (sys.argv) == 1:
-        print('Usage:  select_dark L0_FILE')
+    parser = argparse.ArgumentParser(description='Select the appropriate Level 0 dark file')
+    parser.add_argument ('--window', metavar='HOURS', default=36.0, type=float,
+                         help="Acceptable time offset in hours")
+    parser.add_argument ('level0_file', help="Path to Level 0 file needing dark correction")
+    if len(sys.argv)==1:
+        parser.print_usage(sys.stderr)
         sys.exit(0)
+    args = parser.parse_args()
 
-    level0_file = sys.argv[1]
-    level0_keys = get_level0_keys (level0_file)
+    if (args.window <= 0):
+        print ('*** {}: invalid window = {}'.format(os.path.basename(sys.argv[0]), args.window))
+        sys.exit(1)
+
+    level0_keys = get_level0_keys (args.level0_file)
 
     db_path = get_db_path()
 
@@ -74,8 +89,9 @@ def main():
     conn = sqlite3.connect (db_path)
     conn.execute("pragma foreign_keys=on")
     with conn:
+        #conn.set_trace_callback(print)
         c = conn.cursor()
-        darks = select_matching_dark (c, level0_keys)
+        darks = select_matching_dark (c, args.window, level0_keys)
 
     best_match = darks[0]
     print(best_match[0])
--- pipe/src/select_hk.py
+++ pipe/src/select_hk.py
@@ -9,6 +9,7 @@ import sqlite3
 from datetime import date
 from netCDF4 import Dataset as NetCDFFile
 import numpy as np
+import argparse
 
 # python3 will provide file= redirection to stderr
 def eprint(*args, **kwargs):
@@ -38,9 +39,10 @@ def get_level0_keys (filename):
     nc.close()
     return keys
 
-def select_matching_hk (c, keys):
-    tstart = keys["time_coverage_start_since_epoch"]
-    tend = keys["time_coverage_end_since_epoch"]
+def select_matching_hk (c, window_min, keys):
+    tpad = window_min * 60.0
+    tstart = keys["time_coverage_start_since_epoch"] - tpad
+    tend = keys["time_coverage_end_since_epoch"] + tpad
     cmd = "select path from 'HK_L0' where {} < time_coverage_end_since_epoch AND \
                                     time_coverage_start_since_epoch < {};".format(tstart, tend)
     c.execute(cmd)
@@ -49,12 +51,20 @@ def select_matching_hk (c, keys):
     return rows
 
 def main():
-    if len (sys.argv) == 1:
-        print('Usage:  select_hk.py L0_FILE --> returns a sorted list of HK files')
+    parser = argparse.ArgumentParser(description='Select the appropriate Level 0 HK files')
+    parser.add_argument ('--window', metavar='MINUTES', default=2.0, type=float,
+                         help="Time coverage padding in minutes")
+    parser.add_argument ('level0_file', help="Path to relevant Level 0 file")
+    if len(sys.argv)==1:
+        parser.print_usage(sys.stderr)
         sys.exit(0)
+    args = parser.parse_args()
 
-    level0_file = sys.argv[1]
-    level0_keys = get_level0_keys (level0_file)
+    if (args.window <= 0):
+        print ('*** {}: invalid window = {}'.format(os.path.basename(sys.argv[0]), args.window))
+        sys.exit(1)
+
+    level0_keys = get_level0_keys (args.level0_file)
 
     db_path = get_db_path()
 
@@ -68,7 +78,7 @@ def main():
     conn.execute("pragma foreign_keys=on")
     with conn:
         c = conn.cursor()
-        hks = select_matching_hk (c, level0_keys)
+        hks = select_matching_hk (c, args.window, level0_keys)
 
     if len(hks) == 0:
         print("NONE")
