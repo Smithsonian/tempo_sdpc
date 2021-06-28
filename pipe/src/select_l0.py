d23b01943f53187889f708172a158491951003f7
John C. Houck <jhouck@cfa.harvard.edu>
2021-06-28 14:28:48 -0400

    pipe: select_l0.py supports a wait option

--- pipe/src/select_l0.py
+++ pipe/src/select_l0.py
@@ -5,6 +5,8 @@
 
 import os, sys
 import time
+import signal
+from threading import Event
 import sqlite3
 from datetime import date
 from netCDF4 import Dataset as NetCDFFile
@@ -32,16 +34,50 @@ def get_keys (filename):
     return keys
 
 def select_matching (c, table_name, tstart, tend):
-    cmd = "select path from '{}' where {} < time_coverage_end_since_epoch AND \
+    cmd = "select path,time_coverage_end_since_epoch from '{}' where {} < time_coverage_end_since_epoch AND \
                                     time_coverage_start_since_epoch < {};".format(table_name, tstart, tend)
     c.execute(cmd)
     rows = c.fetchall()
     rows = [r for r in rows if None not in r]
     return rows
 
+def db_connect (db_path):
+    """
+     For back-compatibility sqlite has foreign keys turned off by default,
+     and foreign_keys=off is ALWAYS stored in the database, regardless of
+     the runtime setting when the database was created.  For this reason,
+     we apparently need to turn it on explicitly, each time the database
+     connection is established.
+    """
+    conn = sqlite3.connect (db_path)
+    conn.execute("pragma foreign_keys=on")
+    #conn.set_trace_callback(print)
+    return conn
+
+class Signal_Catcher:
+
+  exit = None
+  signum = None
+
+  def __init__(self):
+    self.exit = Event()
+    signal.signal(signal.SIGINT, self.handler)
+    signal.signal(signal.SIGHUP, self.handler)
+    signal.signal(signal.SIGTERM, self.handler)
+
+  def wait(self, delay):
+      self.exit.wait(delay)
+
+  def caught(self):
+      return self.exit.is_set()
+
+  def handler(self,signum, frame):
+    self.exit.set()
+    self.signum = signum
+
 def main():
     parser = argparse.ArgumentParser(description='Select the appropriate Level 0 files')
-    parser.add_argument ('--wait', type=float, default=60.0, help="Optional wait time")
+    parser.add_argument ('--wait', type=float, default=0.0, help="Optional wait time")
     parser.add_argument ('--table', help="Product table name")
     parser.add_argument ('--begin', type=float, help="start time")
     parser.add_argument ('--end', type=float, help="end time")
@@ -53,7 +89,7 @@ def main():
         sys.exit(0)
     args = parser.parse_args()
 
-    if (args.window <= 0):
+    if (args.window < 0):
         print ('*** {}: invalid window = {}'.format(os.path.basename(sys.argv[0]), args.window))
         sys.exit(1)
 
@@ -74,36 +110,35 @@ def main():
 
     db_path = get_db_path()
 
-    """
-     For back-compatibility sqlite has foreign keys turned off by default,
-     and foreign_keys=off is ALWAYS stored in the database, regardless of
-     the runtime setting when the database was created.  For this reason,
-     we apparently need to turn it on explicitly, each time the database
-     connection is established.
-    """
+    # Try to find files to span the entire time interval.
+    # When the DB is being actively updated, wait and retry might help.
 
-    while num_tries_remaining > 0:
-        num_tries_remaining = num_tries_remaining - 1
+    sig = Signal_Catcher()
+    files = []
+
+    while num_tries_remaining > 0 and not sig.caught():
         try:
-            with sqlite3.connect (db_path) as conn:
-                conn.execute("pragma foreign_keys=on")
+            with db_connect (db_path) as conn:
                 c = conn.cursor()
-                files = select_matching (c, args.table, tbeg, tend)
+                sel = select_matching (c, args.table, tbeg, tend)
         except:
-            files = []
-        if len(files) > 0:
-            break
+            sel = []
+        if len(sel) > 0:
+            files = [s[0] for s in sel]
+            tlast = [s[1] for s in sel]
+            if tlast[-1] > tend:
+                break
+        num_tries_remaining = num_tries_remaining - 1
         if args.wait > 0:
-            time.sleep (args.wait)
+            sig.wait (args.wait)
 
     if len(files) == 0:
         print("NONE")
     else:
-        path_list = list(map (' '.join, files))
-        basenames = [os.path.basename(p) for p in path_list]
+        basenames = [os.path.basename(p) for p in files]
         indices = np.argsort(basenames)
         for i in indices:
-            print(path_list[i])
+            print(files[i])
 
 if __name__ == "__main__":
     main()
