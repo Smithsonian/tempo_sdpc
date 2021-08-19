af98e9dffaa6cebdaa143c417b878be7ddec9554
John C. Houck <jhouck@cfa.harvard.edu>
2021-08-19 14:37:30 -0400

    pipe: improve error checking in node setup script

--- pipe/setup/pipe_setup_nodes.sh
+++ pipe/setup/pipe_setup_nodes.sh
@@ -11,15 +11,31 @@ fi
 _run_dir_master=$1
 shift
 
+if ! test -d $_run_dir_master ; then
+    printf "*** Cannot find directory SDPC_RUN_DIR_MASTER = $_run_dir_master\n"
+    exit 1
+fi
+
 _root_dir=$1
 shift
+
+if ! test -d $_root_dir ; then
+    printf "*** Cannot find directory SDPC_ROOT = $_root_dir\n"
+    exit 1
+fi
 
 _node_list="$@"
 
 _pipe_name=$(basename $_run_dir_master)
-
 export SDPC_PIPE_NAME=$_pipe_name
-. $_root_dir/etc/sdpc_env.sh
+
+env_path="$_root_dir/etc/sdpc_env.sh"
+if ! test -f "$env_path" ; then
+   printf "Cannot find script file: $env_path\n"
+   exit 1
+fi
+
+. "$env_path"
 
 for node in $_node_list; do
   printf "Creating pipeline node directory: $node:$SDPC_RUN_DIR\n"
