f99699ad24c72e5f04b1a8e28efd37386cb47f1c
John C. Houck <jhouck@cfa.harvard.edu>
2021-10-30 08:03:05 -0400

    Update INSTALL instructions to refer to user manual.

---
+++ sdpc_configure.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+compilers=gnu
+
+install_root="/tempo/nas0/sdpc_soft/install/$compilers"
+refdata_dir="/tempo/nas0/sdpc_soft/refdata"
+ancdata_dir="/tempo/nas0/sdpc/ancillary"
+
+prefix="$install_root/sdpc/v3"
+inrroot="$install_root/inr_r2.2.2"
+otsroot="$install_root/ots"
+s6root="$install_root/skarnet"
+
+./configure --prefix=$prefix \
+            --with-compilers=$compilers \
+            --with-otsroot=$otsroot \
+            --with-slang=$otsroot \
+            --with-inrroot=$inrroot \
+            --with-s6root=$s6root \
+            --with-refdata=$refdata_dir \
+            --with-ancdata=$ancdata_dir \
+            --with-atlasblas
