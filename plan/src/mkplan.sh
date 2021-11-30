4d4c5336ed8ce090fe95ec4084368fa69de1b258
John C. Houck <jhouck@cfa.harvard.edu>
2021-11-30 10:49:13 -0500

    plan: add mkplan.sh options to print config files

--- plan/src/mkplan.sh
+++ plan/src/mkplan.sh
@@ -1,17 +1,39 @@
 #! /bin/sh
 
-PGMNAME="mkplan.sh"
+PGMNAME="$(basename $0)"
 
 if ! test $# -eq 1 ; then
-   echo "Usage: $PGMNAME <schedule-config>"
+   echo "Usage: $PGMNAME <schedule-config> | [options]"
+   echo "   Options:"
+   echo "     --sch     Print schedule configuration template"
+   echo "     --cfg     Print scan configuration template"
    exit 0
 fi
-
-sched_file="$1"
 
 # SDPC_OTS_ROOT is used in the plan.cfg file
 : "${SDPC_OTS_ROOT:?SDPC_OTS_ROOT is not set}"
 : "${SDPC_ROOT:?SDPC_ROOT is not set}"
+
+default_config_file="$SDPC_ROOT/share/plan.cfg"
+example_sched_file="$SDPC_ROOT/share/plan_sched.sh.example"
+
+case "$1" in
+   --sch)
+     if test -f $example_sched_file ; then
+        cat $example_sched_file
+        exit 0
+     fi
+     ;;
+   --cfg)
+     if test -f $default_config_file ; then
+        cat $default_config_file
+        exit 0
+     fi
+     ;;
+   *)
+     sched_file="$1"
+     ;;
+esac
 
 if test -r "$sched_file" ; then
    . $sched_file
@@ -25,7 +47,7 @@ if test x"$prefix" = x ; then
 fi
 
 if test x"$config_file" = x ; then
-   config_file="$SDPC_ROOT/share/plan.cfg"
+   config_file="$default_config_file"
 fi
 
 : "${maneuver_file:?maneuver_file is not defined}"
@@ -47,16 +69,16 @@ if ! test -d $target_dir ; then
    mkdir -p $target_dir || error_exit "mkdir failed"
 fi
 
-echo "Writing output to $target_dir"
+echo "Writing temporary output to $target_dir"
 
 _tailor="${target_dir}/${prefix}_scantailor.csv"
 _master="${target_dir}/${prefix}_masterscan_tbl.csv"
 _plan="${target_dir}/${prefix}_earthscan_tbl.csv"
 
 # generate master scan table, and scan plan
-plan -v -c $config_file -M $maneuver_file \
+plan -c $config_file -M $maneuver_file \
      -s $plan_type -d $plan_start_day -n $plan_num_days \
-     -o $_plan \
+     $plan_options -o $_plan \
      -m $_master || error_exit "failed generating scan plan"
 
 /bin/cp $config_file $target_dir/${prefix}_plan.cfg
@@ -64,6 +86,9 @@ plan -v -c $config_file -M $maneuver_file \
 
 # generate scan tailoring file
 if test x"$tailoring_file" = x ; then
+: "${tailor_epoch:?tailor_epoch is not defined}"
+: "${tailor_start_day:?tailor_start_day is not defined}"
+: "${tailor_num_days:?tailor_num_days is not defined}"
    plan -c $config_file --epoch $tailor_epoch \
         -d $tailor_start_day -n $tailor_num_days \
         -T $_tailor || error_exit "failed generating scan tailoring file"
@@ -73,9 +98,13 @@ else
   error_exit "cannot read $tailoring_file"
 fi
 
+tarfile="${prefix}.tar.gz"
+
 if test -d $target_dir ; then
-  tar cvzf ${prefix}.tar.gz --remove-files -C $(dirname $target_dir) ${prefix}
+  tar czf $tarfile --remove-files -C $(dirname $target_dir) ${prefix}
   if test -d $out_dir ; then
     /bin/rmdir $out_dir
   fi
 fi
+
+echo "Created $tarfile"
