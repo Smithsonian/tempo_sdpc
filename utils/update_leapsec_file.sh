19f0eb16eccdb5abd7888b6a5dee5e59db04ecd7
John C. Houck <jhouck@cfa.harvard.edu>
2021-12-03 09:53:20 -0500

    utils: leapsec update script performs atomic update

--- utils/update_leapsec_file.sh
+++ utils/update_leapsec_file.sh
@@ -5,11 +5,14 @@ set -e
 
 LEAP_FILE="leap-seconds.list"
 
-if test -f $LEAP_FILE ; then
-  suffix=$(date --iso-8601)
-  newfile="${LEAP_FILE}.${suffix}"
-  /bin/mv $LEAP_FILE $newfile
-  printf "moved existing file to $newfile\n"
-fi
+target=$(mktemp)
+wget -q https://www.ietf.org/timezones/data/leap-seconds.list -O $target
 
-wget -q https://www.ietf.org/timezones/data/leap-seconds.list
+if test -f $LEAP_FILE ; then
+  bkpfile="${LEAP_FILE}.$(date -u +%Y-%m-%dT%H:%M:%SZ)"
+  /bin/cp -a $LEAP_FILE $bkpfile
+  printf "existing file copied to $bkpfile\n"
+fi
+chmod ugo+r $target
+/bin/mv $target $LEAP_FILE
+printf "updated $LEAP_FILE\n"
