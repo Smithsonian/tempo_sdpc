bb86fe6f0a8183423c7354e3f5fe9c4f01850b69
John C. Houck <jhouck@cfa.harvard.edu>
2022-01-01 07:25:26 -0500

    utils: add sdpc_logview.sh utility

---
+++ utils/sdpc_logview.sh
@@ -0,0 +1,115 @@
+#! /bin/sh
+
+check_cmd_exists()
+{
+   cmd="$1"
+   if ! command -v $cmd &> /dev/null ; then
+      echo "*** Error: $cmd not found"
+      exit 1
+   fi
+}
+
+check_cmd_exists multitail
+check_cmd_exists whiptail
+
+exit_usage()
+{
+   echo "Usage: $(basename $0) [options] [service [service] ...]"
+   echo "   Options:"
+   echo "   --help     Print this listing"
+   echo "   --dir      Primary node pipeline directory [default=\$SDPC_RUN_DIR_MASTER]"
+   echo "   --menu     Display service menu"
+   exit "$1"
+}
+
+default_logdirs()
+{
+  logdirs="level1a inr level1b level2 level3 register asdc"
+}
+
+choose_logdirs()
+{
+   read -r -d '' itemlist <<-'EOF'
+	iocpull off
+	iocpullraw off
+	level0 off
+	level1a on
+	inr on
+	level1b on
+	level2 on
+	level3 on
+	trend off
+	register on
+	asdc on
+	EOF
+
+  num_items=$(echo $itemlist | wc -w)
+  num_items=$((num_items / 2))
+
+  num_lines=$((num_items + 10))
+  num_cols=32
+
+  logdirs=$(
+  whiptail --checklist "Choose which logs to view:" \
+           --title "SDPC services" \
+           --noitem \
+           $num_lines $num_cols $num_items $itemlist 3>&2 2>&1 1>&3
+  )
+  status="$?"
+  if test $status = 1; then
+     exit 0
+  elif ! test $status = 0 ; then
+     exit $status
+  fi
+}
+
+main()
+{
+  default_logdirs
+
+  if ! test -z "$SDPC_RUN_DIR_MASTER" ; then
+     root="$SDPC_RUN_DIR_MASTER"
+  fi
+
+  # Process optional args
+  while [ "$#" != "0" ]
+  do
+     case "$1" in
+       --*)
+         case "$1" in
+           --help)
+             exit_usage 0
+             ;;
+	   --dir)
+	      shift
+ 	      root="$1"
+	      shift
+              if ! test -d "$root" ; then
+                 echo "Nonexistent directory: $root"
+                 exit 1
+              fi
+	      ;;
+           --menu)
+              shift;
+              choose_logdirs
+              ;;
+           *)
+             echo "Unknown option: $1"
+             exit 1
+             ;;
+         esac
+         ;;
+       *)
+         logdirs="$@"
+         break
+         ;;
+     esac
+  done
+
+  logfiles=$(printf "$root/log/%s/current " $logdirs | tr -d \")
+
+  multitail -o default_convert:qmailtimestr:current \
+          -v -E TIO_TRACE $logfiles
+}
+
+main "$@"
