3f529fd7782832d46395d7fb8e4f3f9174f38c11
hwang <hwang@cfa.harvard.edu>
2021-12-31 22:49:07 -0500

    L1_cloud_o4: remove the following unused subroutines from m_read_hdf5.f90    read_BRDF_Rsfc_h5, read_BDEM_Psfc_h5

--- L1_cloud_o4/src/m_read_hdf5.f90
+++ L1_cloud_o4/src/m_read_hdf5.f90
@@ -79,183 +79,24 @@ subroutine read_GEOS5_VCD(pp,tt)
 end subroutine read_GEOS5_VCD
 !22222222222222222222222222222
 
+!hqw GLER is read in m_read_input_gler.f90
+!the following is no longer needed, thus removed
 !5555555555555555555555555555
-subroutine read_BRDF_Rsfc_h5
+!subroutine read_BRDF_Rsfc_h5
 !5555555555555555555555555555
-  use m_vars
-  use HDF5
-
-  IMPLICIT NONE
-
-  CHARACTER(LEN=255)::filename
-  CHARACTER(LEN=255)::dsetname
-
-! HID_T type integers.
-  INTEGER(HID_T)::file_id,dset_id,datatype_id
-
-! Regular four-byte integer.
-  INTEGER(KIND=4)::hdf_err,ierr
-  real(kind=4),dimension(:,:,:),allocatable::BRDF_SurfaceReflectivity
-
-! HSIZE_T type integer.
-  !INTEGER(HSIZE_T),DIMENSION(1)::dims1
-  !INTEGER(HSIZE_T),DIMENSION(2)::dims2
-  INTEGER(HSIZE_T),DIMENSION(3)::dims3
-
-  integer(kind=4)::nt,nx,nw,it,ix
-
-!-------
-! start
-!-------
-  filename=trim(name_brdf_dir)//trim(name_brdf_file)
-
-  !hqw replace
-  !nt=inp_NumTimes
-  !nx=inp_nXtrack
-  nt = rad_NumTimes
-  nx = rad_nXtrack
-  nw=2
-  
-! allocate dimensions & fill values for outputs
-!hqw BRDF_SurfaceReflectivity is local, will be deallocated at end
-  allocate(BRDF_SurfaceReflectivity(nw,nx,nt),stat=ierr)
-  BRDF_SurfaceReflectivity=-9.9
-  allocate(BRDF_SurfaceReflectivity466(nx,nt),stat=ierr)
-  BRDF_SurfaceReflectivity466=-9.9
-  allocate(BRDF_SurfaceReflectivity440(nx,nt),stat=ierr)
-  BRDF_SurfaceReflectivity440=-9.9
-
-! Initialize FORTRAN interface.
-  CALL h5open_f(hdf_err)
-
-! Open an existing file.
-  CALL h5fopen_f(filename,H5F_ACC_RDONLY_F,file_id,hdf_err)
-if (hdf_err /= 0) print *, "read_BRDF_Rsfc_h5 fail"
-
-! Open an existing dataset.
-  dsetname="/HDFEOS/SWATHS/Geometry Dependent Surface LER/Data Fields/GLER"
-  CALL h5dopen_f(file_id,dsetname,dset_id,hdf_err)
-  CALL h5dget_type_f(dset_id,datatype_id,hdf_err)
-  dims3=SHAPE(BRDF_SurfaceReflectivity)
-  CALL H5Dread_f(dset_id,datatype_id,BRDF_SurfaceReflectivity,dims3,hdf_err)
-  CALL h5dclose_f(dset_id,hdf_err)
-    
-! Close the file.
-  CALL h5fclose_f(file_id,hdf_err)
-
-! Close FORTRAN interface.
-  CALL h5close_f(hdf_err)
-
-  do it=1,nt
-  do ix=1,nx
-    BRDF_SurfaceReflectivity466(ix,it)=BRDF_SurfaceReflectivity(2,ix,it)
-    BRDF_SurfaceReflectivity440(ix,it)=BRDF_SurfaceReflectivity(1,ix,it)
-  end do
-  end do
-
-  !hqw deallocate helpers
-  deallocate(BRDF_SurfaceReflectivity)
  
 !55555555555555555555555555555555
-end subroutine read_BRDF_Rsfc_h5
+!end subroutine read_BRDF_Rsfc_h5
 !55555555555555555555555555555555
 
 !hqw GLER now handled by m_read_input_gler.f90
-!the following is no longer used 
+!the following is no longer used, thus removed
 !6666666666666666666666666666
-subroutine read_BDEM_Psfc_h5
+!subroutine read_BDEM_Psfc_h5
 !6666666666666666666666666666
-  use m_vars
-  use HDF5
-  IMPLICIT NONE
-
-  CHARACTER(LEN=255)::filename
-  CHARACTER(LEN=255)::dsetname
-
-! HID_T type integers.
-  INTEGER(HID_T)::file_id,dset_id,datatype_id
-
-! Regular four-byte integer.
-  INTEGER(KIND=4)::hdf_err,ierr
-
-! HSIZE_T type integer.
-  !INTEGER(HSIZE_T),DIMENSION(1)::dims1
-  INTEGER(HSIZE_T),DIMENSION(2)::dims2
-  !INTEGER(HSIZE_T),DIMENSION(3)::dims3
-
-  integer(kind=4)::nt,nx!,it,ix
-
-!-------
-! start
-!-------
-  filename=trim(name_brdf_dir)//trim(name_brdf_file)
-
-  !hqw replace nt, nx as inp_ were from OMCLDO2
-  !nt=inp_NumTimes
-  !nx=inp_nXtrack
-  nt = rad_NumTimes
-  nx = rad_nXtrack
-  
-! allocate dimensions & fill values for outputs
-! hqw STDev & LandAreaFraction are not actually used in calculation,
-! thus, comment out to save memory
-  allocate(BDEM_TerrainPressure(nx,nt),stat=ierr)
-!  allocate(BDEM_TerrainPressureStdDev(nx,nt),stat=ierr)
-  allocate(BDEM_TerrainHeight(nx,nt),stat=ierr)
-!  allocate(BDEM_TerrainHeightStdDev(nx,nt),stat=ierr)
-!  allocate(BDEM_LandAreaFraction(nx,nt),stat=ierr)
-
-! Initialize FORTRAN interface.
-  CALL h5open_f(hdf_err)
-
-! Open an existing file.
-  CALL h5fopen_f(filename,H5F_ACC_RDONLY_F,file_id,hdf_err)
-if (hdf_err /= 0) print *, "read_BDEM_Psfc_h5 fail"
-
-! Open an existing dataset.
-  dsetname="/HDFEOS/SWATHS/Geometry Dependent Surface LER/Data Fields/TerrainPressure"
-  CALL h5dopen_f(file_id,dsetname,dset_id,hdf_err)
-  CALL h5dget_type_f(dset_id,datatype_id,hdf_err)
-  dims2=SHAPE(BDEM_TerrainPressure)
-  CALL H5Dread_f(dset_id,datatype_id,BDEM_TerrainPressure,dims2,hdf_err)
-  CALL h5dclose_f(dset_id,hdf_err)
-
-!  dsetname="/HDFEOS/SWATHS/Geometry Dependent Surface LER/Data Fields/TerrainPressureStdDev"
-!  CALL h5dopen_f(file_id,dsetname,dset_id,hdf_err)
-!  CALL h5dget_type_f(dset_id,datatype_id,hdf_err)
-!  dims2=SHAPE(BDEM_TerrainPressureStdDev)
-!  CALL H5Dread_f(dset_id,datatype_id,BDEM_TerrainPressureStdDev,dims2,hdf_err)
-!  CALL h5dclose_f(dset_id,hdf_err)
-
-  dsetname="/HDFEOS/SWATHS/Geometry Dependent Surface LER/Data Fields/TerrainHeight"
-  CALL h5dopen_f(file_id,dsetname,dset_id,hdf_err)
-  CALL h5dget_type_f(dset_id,datatype_id,hdf_err)
-  dims2=SHAPE(BDEM_TerrainHeight)
-  CALL H5Dread_f(dset_id,datatype_id,BDEM_TerrainHeight,dims2,hdf_err)
-  CALL h5dclose_f(dset_id,hdf_err)
-
-!  dsetname="/HDFEOS/SWATHS/Geometry Dependent Surface LER/Data Fields/TerrainHeightStdDev"
-!  CALL h5dopen_f(file_id,dsetname,dset_id,hdf_err)
-!  CALL h5dget_type_f(dset_id,datatype_id,hdf_err)
-!  dims2=SHAPE(BDEM_TerrainHeightStdDev)
-!  CALL H5Dread_f(dset_id,datatype_id,BDEM_TerrainHeightStdDev,dims2,hdf_err)
-!  CALL h5dclose_f(dset_id,hdf_err)
-
-!  dsetname="/HDFEOS/SWATHS/Geometry Dependent Surface LER/Data Fields/LandAreaFraction"
-!  CALL h5dopen_f(file_id,dsetname,dset_id,hdf_err)
-!  CALL h5dget_type_f(dset_id,datatype_id,hdf_err)
-!  dims2=SHAPE(BDEM_LandAreaFraction)
-!  CALL H5Dread_f(dset_id,datatype_id,BDEM_LandAreaFraction,dims2,hdf_err)
-!  CALL h5dclose_f(dset_id,hdf_err)
-
-! Close the file.
-  CALL h5fclose_f(file_id,hdf_err)
-
-! Close FORTRAN interface.
-  CALL h5close_f(hdf_err)
 
 !66666666666666666666666666666666
-end subroutine read_BDEM_Psfc_h5
+!end subroutine read_BDEM_Psfc_h5
 !66666666666666666666666666666666
 
 !**********************
