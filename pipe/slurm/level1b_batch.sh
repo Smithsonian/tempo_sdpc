93422ae4f8eab7f48bb7d57f65479a5cc379dc9f
John C. Houck <jhouck@cfa.harvard.edu>
2022-01-04 16:25:23 -0500

    pipe: updates associated with L1_cloud_o4 update

--- pipe/slurm/level1b_batch.sh
+++ pipe/slurm/level1b_batch.sh
@@ -406,8 +406,15 @@ derive_cloud_o4_params()
    srun --ntasks=1 --output=log_cloud_o4.txt \
         L1_cloud_o4 $ctrl_file
 
-   ncks -x -v main_data_quality_flag $product_file ${product_file}.tmp
+   # remove variables:
+   #        - main_data_quality_flag
+   #        - surface_pressure
+   ncks -x -v main_data_quality_flag \
+        -x -v surface_pressure $product_file ${product_file}.tmp
+   # rename SurfacePressure -> surface_pressure
+   ncrename -v SurfacePressure,surface_pressure ${product_file}.tmp
    /bin/mv ${product_file}.tmp $product_file
+
 }
 
 run_cloud_o4()
