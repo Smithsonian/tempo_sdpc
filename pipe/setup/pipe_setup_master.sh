f94e5a59ef81f0f59ab3ce700c74bb74c22cdfd0
John C. Houck <jhouck@cfa.harvard.edu>
2022-05-03 08:52:51 -0400

    pipe: select context using pipe_setup_master.sh command line option

--- pipe/setup/pipe_setup_master.sh
+++ pipe/setup/pipe_setup_master.sh
@@ -7,8 +7,103 @@ set -e
 : "${SDPC_ROOT:?SDPC_ROOT not set -- source sdpc_env.sh}"
 : "${SDPC_RUN_DIR_MASTER:?SDPC_RUN_DIR_MASTER not set -- source sdpc_env.sh}"
 
-pipe_mkdirs_master.sh
+exit_usage()
+{
+   echo "Usage: $(basename $0) [options]"
+   echo "   Options:"
+   echo "   --help     Print this listing"
+   echo "   --config   Pipeline context: live | cache | repro0 | repro1"
+   exit "$1"
+}
+
+list_up=""
+list_down=""
+
+service_states_for_context()
+{
+   context="$1"
+   ioc_srvs="iocpull iocpullraw level0"
+   lev1a_srvs="level1a inr trend"
+   always_up="level1b level2 level3 register pipecron asdc"
+   case "$context" in
+      live)
+      list_up="$ioc_srvs $lev1a_srvs $always_up"
+      list_down=""
+      ;;
+      cache)
+      list_up="$lev1a_srvs $always_up"
+      list_down="$ioc_srvs"
+      ;;
+      repro0)
+      list_up="$lev1a_srvs $always_up"
+      list_down="$ioc_srvs"
+      ;;
+      repro1)
+      list_up="$always_up"
+      list_down="$ioc_srvs $lev1a_srvs"
+      ;;
+      *)
+      echo "*** Error: unsupported pipeline context: $context"
+      exit 1
+      ;;
+   esac
+}
+
+service_default_up()
+{
+   list="$1"
+   if test x"$list" = x ; then
+      return
+   fi
+   for srv in $list ; do
+       /bin/rm -f $SDPC_RUN_DIR_MASTER/services/$srv/down
+   done
+   echo "Services up: $list"
+}
+
+service_default_down()
+{
+   list="$1"
+   if test x"$list" = x ; then
+      return
+   fi
+   for srv in $list ; do
+       touch $SDPC_RUN_DIR_MASTER/services/$srv/down
+   done
+   echo "Services down: $list"
+}
+
+# Process optional args
+while [ "$#" != "0" ]
+do
+   case "$1" in
+     --*)
+       case "$1" in
+         --help)
+           exit_usage 0
+           ;;
+         --config)
+           shift
+           if test $# = 0 ; then
+              echo "*** Error: missing config argument"
+              exit 1
+           fi
+           service_states_for_context "$1"
+           shift
+           ;;
+         --*)
+           exit_usage 0
+           ;;
+       esac
+       ;;
+     *)
+       exit_usage 0
+     ;;
+   esac
+done
+
 pipe_mkdirs_archive.sh
+pipe_mkdirs_master.sh
 
 inr_mkdirs.sh
 inr_config.sh
@@ -16,3 +111,5 @@ inr_config.sh
 /bin/cp -r $SDPC_ROOT/etc $SDPC_RUN_DIR_MASTER
 /bin/mv $SDPC_RUN_DIR_MASTER/etc/services $SDPC_RUN_DIR_MASTER
 
+service_default_up "$list_up"
+service_default_down "$list_down"
