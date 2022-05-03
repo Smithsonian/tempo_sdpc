388e47924dab335a79c4c7ad78aa5969d2f9f174
John C. Houck <jhouck@cfa.harvard.edu>
2022-05-03 15:05:17 -0400

    pipe: add pull_ioc_cache.sh script

---
+++ pipe/src/pull_ioc_cache.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+
+if test -z "${SDPC_ROOT}"
+then
+  echo "The SDPC environment is not set"
+  exit 1
+fi
+
+exit_usage()
+{
+   echo "Usage: $(basename $0) [options] <dirlist-file>"
+   echo "   Options:"
+   echo "   --help              Print this listing"
+   exit "$1"
+}
+
+run_l0_format()
+{
+   cachedir_list="$1"
+
+   logdir="$SDPC_RUN_DIR_MASTER/log/level0"
+   if ! test -d $logdir ; then
+      echo "*** Error: cannot access log directory: $logdir"
+      return
+   fi
+   echo "Running L0_format, log directory: $logdir"
+
+   L0_format --archive "$SDPC_ARCHIVE_DIR" --register --verbose \
+             --cache @${cachedir_list} \
+             --Version "$SDPC_PROCESSING_VERSION" \
+             --logdir "$logdir" \
+             "$SDPC_RUN_DIR_MASTER/etc/l0_format.cfg" > "$logdir/l0_format.$$.log" 2>&1
+}
+
+main()
+{
+   if test $# -eq 0 || test $# -gt 1 ; then
+      exit_usage 0
+   fi
+
+   cachedir_list="$1"
+
+   if ! test -f $cachedir_list ; then
+      echo "*** Error: cannot read file: $cachedir_list"
+      exit 1
+   fi
+
+   run_l0_format $cachedir_list
+}
+
+main "$@"
