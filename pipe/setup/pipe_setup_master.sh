065c07a6f3a4cc3e15413a80e92a55a53ceaf3c4
John C. Houck <jhouck@cfa.harvard.edu>
2022-05-04 08:53:44 -0400

    pipe: iocpullraw should default to 'up' during IOC cache processing

--- pipe/setup/pipe_setup_master.sh
+++ pipe/setup/pipe_setup_master.sh
@@ -22,25 +22,25 @@ list_down=""
 service_states_for_context()
 {
    context="$1"
-   ioc_srvs="iocpull iocpullraw level0"
+   ioc_srvs="iocpull level0"
    lev1a_srvs="level1a inr trend"
    always_up="level1b level2 level3 register pipecron asdc"
    case "$context" in
       live)
-      list_up="$ioc_srvs $lev1a_srvs $always_up"
+      list_up="$ioc_srvs iocpullraw $lev1a_srvs $always_up"
       list_down=""
       ;;
       cache)
-      list_up="$lev1a_srvs $always_up"
+      list_up="$lev1a_srvs iocpullraw $always_up"
       list_down="$ioc_srvs"
       ;;
       repro0)
       list_up="$lev1a_srvs $always_up"
-      list_down="$ioc_srvs"
+      list_down="$ioc_srvs iocpullraw"
       ;;
       repro1)
       list_up="$always_up"
-      list_down="$ioc_srvs $lev1a_srvs"
+      list_down="$ioc_srvs iocpullraw $lev1a_srvs"
       ;;
       *)
       echo "*** Error: unsupported pipeline context: $context"
