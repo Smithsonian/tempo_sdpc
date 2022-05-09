2858042d9ecb1b08997fdeebf8e8635cf15d6ecc
John C. Houck <jhouck@cfa.harvard.edu>
2022-05-09 06:11:03 -0400

    utils: check for RAW in table list before trying to remove it

--- utils/asdc_upload_report.py
+++ utils/asdc_upload_report.py
@@ -25,7 +25,8 @@ def get_product_table_names (cur):
     cur.execute ("select name from sqlite_master where type = 'table' and name not like 'sqlite_%'");
     table_names = [item for t in cur.fetchall() for item in t]
     if table_names is not None:
-        table_names.remove('RAW')
+        if 'RAW' in table_names:
+            table_names.remove('RAW')
         table_names.sort(key = lambda x: x.split("_")[1])
     return table_names
 
