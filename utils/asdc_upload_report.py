9a0a4646d605f214da6c37ed1cb6aa173e81753c
John C. Houck <jhouck@cfa.harvard.edu>
2022-05-13 11:09:33 -0400

    utils: add columns to ASDC upload report

--- utils/asdc_upload_report.py
+++ utils/asdc_upload_report.py
@@ -9,6 +9,7 @@ import re
 import argparse
 
 Asdc_Status = {"nonexistent":-2, "problem":-1, "new": 0, "pending":1, "uploaded":2, "accepted":3}
+Asdc_Status_Lookup = {value: key for key, value in Asdc_Status.items()}
 
 def connect_database ():
     db_file_path = os.getenv ("SDPC_ARCHIVE_DBFILE")
@@ -24,14 +25,62 @@ def connect_database ():
 def get_product_table_names (cur):
     cur.execute ("select name from sqlite_master where type = 'table' and name not like 'sqlite_%'");
     table_names = [item for t in cur.fetchall() for item in t]
-    if table_names is not None:
-        if 'RAW' in table_names:
-            table_names.remove('RAW')
-        table_names.sort(key = lambda x: x.split("_")[1])
+    if table_names is None:
+        return None
+    # 'RAW' doesn't fit the table name pattern.
+    append_raw = 'RAW' in table_names
+    if append_raw:
+        table_names.remove('RAW')
+    table_names.sort(key = lambda x: x.split("_")[1])
+    if append_raw:
+        table_names.append('RAW')
     return table_names
 
-def table_accepted_summary (cur, table_name, tbounds):
+def table_accepted_summary (cur, table_name, tbeg, tend):
     accepted = Asdc_Status["accepted"]
+    sql = "select avg(asdc_upload_time-mtime),avg(asdc_ingest_time-mtime),avg(size),count(*),sum(size) from {table_name} where mtime > {tbeg} and mtime < {tend} and asdc_status == {accepted}".format(**locals())
+    cur.execute (sql)
+    results = [item for t in cur.fetchall() for item in t]
+    return {'upload_time':results[0], 'ingest_time':results[1], 'size':results[2], 'count':results[3], 'total_size':results[4]}
+
+def table_status_summary (cur, table_name, tbeg, tend):
+    sql = "select asdc_status,count(*) from {table_name} where mtime > {tbeg} and mtime < {tend} group by asdc_status".format(**locals())
+    cur.execute (sql)
+    results = cur.fetchall()
+    # status_count = dict with Asdc_Status keys, with values initialized to zero
+    status_count = dict.fromkeys(Asdc_Status, 0)
+    for r in results:
+        if r[0] in Asdc_Status_Lookup:
+            status_count[Asdc_Status_Lookup[r[0]]] = r[1]
+        else:
+            eprint('*** Error: invalid asdc_status value: {}'.format(r[0]))
+            sys.exit(1)
+    return status_count
+
+def print_table_summaries (table_list, tbeg, tend):
+    print ("#")
+    if tbeg > 0:
+        print("# Start time: %s" % (time.strftime ('%Y-%m-%dT%H:%M:%SZ', time.gmtime(tbeg))))
+    print("#   End time: %s" % (time.strftime ('%Y-%m-%dT%H:%M:%SZ', time.gmtime(tend))))
+    print ("#")
+    with connect_database() as conn:
+        cur = conn.cursor()
+        if table_list is None:
+            table_list = get_product_table_names (cur)
+        print ("#  Product        mean        mean       mean    total     files   ingest   ingest")
+        print ("#    table  age_upload  age_ingest       size     size  accepted  pending  problem")
+        print ("#                [min]       [min]       [MB]     [GB]                            ")
+        for table in table_list:
+            status_count = table_status_summary (cur, table, tbeg, tend)
+            summary = table_accepted_summary (cur, table, tbeg, tend)
+            if summary["count"] == 0:
+                summary = {'upload_time':-1, 'ingest_time':-1, 'size':-1, 'count':0, 'total_size':-1}
+            print ("%10s  %10.1f  %10.1f %9.1f %9.1f   %7d  %7d  %7d" % (table,
+                   summary["upload_time"]/60.0, summary["ingest_time"]/60.0, summary["size"]/1.e6,
+                   summary["total_size"]/1.e9, summary["count"],
+                   status_count["uploaded"], status_count["problem"]))
+
+def tlimits_from_tbounds (tbounds):
     if tbounds is not None:
         tbeg_obj = dateutil.parser.isoparse(tbounds[0])
         tend_obj = dateutil.parser.isoparse(tbounds[1])
@@ -40,28 +89,7 @@ def table_accepted_summary (cur, table_name, tbounds):
     else:
         tbeg = 0
         tend = int(time.time())
-    sql = "select avg(asdc_upload_time-mtime),avg(asdc_ingest_time-mtime),avg(size),count(*) from {table_name} where mtime > {tbeg} and mtime < {tend} and asdc_status == {accepted}".format(**locals())
-
-    cur.execute (sql)
-    results = [item for t in cur.fetchall() for item in t]
-
-    return {'upload_time':results[0], 'ingest_time':results[1], 'size':results[2], 'count':results[3]}
-
-def print_table_summaries (table_list, tbounds):
-    with connect_database() as conn:
-        cur = conn.cursor()
-        if table_list is None:
-            table_list = get_product_table_names (cur)
-        print ("#  Product        mean        mean       mean    file")
-        print ("#    table  age_upload  age_ingest       size   count")
-        print ("#                [min]       [min]       [MB]        ")
-        for table in table_list:
-            summary = table_accepted_summary (cur, table, tbounds)
-            if summary["upload_time"] is None or summary["ingest_time"] is None:
-                continue;
-            print ("%10s  %10.1f  %10.1f %9.1f  %7d" % (table,
-            summary["upload_time"]/60.0, summary["ingest_time"]/60.0, summary["size"]/1.e6,
-            summary["count"]))
+    return tbeg, tend
 
 def main():
     parser = argparse.ArgumentParser(description='Summarize ASDC uploads')
@@ -74,7 +102,9 @@ def main():
     #     sys.exit(0)
     args = parser.parse_args()
 
-    print_table_summaries (args.tables, args.interval)
+    tbeg, tend = tlimits_from_tbounds (args.interval)
+
+    print_table_summaries (args.tables, tbeg, tend)
 
 if __name__ == "__main__":
     main()
