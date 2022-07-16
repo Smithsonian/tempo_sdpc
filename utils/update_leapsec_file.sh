7c7194b9c4c04f099006994cc94532ce99ac49c5
John C. Houck <jhouck@cfa.harvard.edu>
2022-07-16 15:11:12 -0400

    utils: leapsec update replaces the file only when the new one is different

--- utils/update_leapsec_file.sh
+++ utils/update_leapsec_file.sh
@@ -9,6 +9,13 @@ target=$(mktemp)
 wget -q https://www.ietf.org/timezones/data/leap-seconds.list -O $target
 
 if test -f $LEAP_FILE ; then
+   old_md5sum=$(md5sum $LEAP_FILE | cut -d' ' -f1)
+   new_md5sum=$(md5sum $target | cut -d' ' -f1)
+   if test x"$old_md5sum" == x"$new_md5sum" ; then
+      /bin/rm $target
+      printf "update not needed\n"
+      exit 0
+  fi
   bkpfile="${LEAP_FILE}.$(date -u +%Y-%m-%dT%H:%M:%SZ)"
   /bin/cp -a $LEAP_FILE $bkpfile
   printf "existing file copied to $bkpfile\n"
