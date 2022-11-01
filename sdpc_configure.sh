b3c81f0262c1cecfc47109a00ae78bd39c477423
John C. Houck <jhouck@cfa.harvard.edu>
2022-11-01 07:30:27 -0400

    add archive_home, pipe_home to configure invocation

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -6,6 +6,9 @@ install_root="/tempo/nas0/sdpc_soft/install/$compilers"
 refdata_dir="/tempo/nas0/sdpc_soft/refdata"
 ancdata_dir="/tempo/nas0/sdpc_archive/ancillary"
 
+archive_home="/tempo/nas0/sdpc_archive"
+pipe_home="/tempo/nas0/sdpc/liveroot"
+
 prefix="$install_root/sdpc/v3"
 inrroot="$install_root/inr_r2.3.5"
 otsroot="$install_root/ots"
@@ -19,4 +22,6 @@ s6root="$install_root/skarnet"
             --with-s6root=$s6root \
             --with-refdata=$refdata_dir \
             --with-ancdata=$ancdata_dir \
-            --with-atlasblas
+            --with-atlasblas \
+            --with-archive-home=$archive_home \
+            --with-pipe-home=$pipe_home
