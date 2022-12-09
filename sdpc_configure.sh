bde63622962a935b4f7cddae50d8a04d48051839
John C. Houck <jhouck@cfa.harvard.edu>
2022-12-09 08:08:09 -0500

    Label as version 4

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -2,14 +2,15 @@
 
 compilers=gnu
 
-install_root="/tempo/nas0/sdpc_soft/install/$compilers"
-refdata_dir="/tempo/nas0/sdpc_soft/refdata"
-ancdata_dir="/tempo/nas0/sdpc_archive/ancillary"
-
+soft_home="/tempo/nas0/sdpc_soft"
 archive_home="/tempo/nas0/sdpc_archive"
 pipe_home="/tempo/nas0/sdpc/liveroot"
 
-prefix="$install_root/sdpc/v3"
+refdata_dir="$soft_home/refdata"
+ancdata_dir="$archive_home/ancillary"
+
+install_root="$soft_home/install/$compilers"
+prefix="$install_root/sdpc/v4"
 inrroot="$install_root/inr_r2.3.5"
 otsroot="$install_root/ots"
 s6root="$install_root/skarnet"
--- utils/get_sdpc_version.sh
+++ utils/get_sdpc_version.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-VN_DEF=3.0.0
+VN_DEF=4.0.0
 
 if test -d .git ; then
    VN=$(git describe --tag  HEAD 2> /dev/null) || VN=$VN_DEF
