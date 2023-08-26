4e0220cad1f3f1d4e15354f3b7dc93ca69528718
John C. Houck <jhouck@cfa.harvard.edu>
2023-08-26 11:26:08 -0400

    pipe: add control parameters to etc/pipe_control.cfg
      * size irradiance select window [days]
      * O3PROF block max run time

--- pipe/slurm/level1b.sh
+++ pipe/slurm/level1b.sh
@@ -54,8 +54,10 @@ rad_basename=$(basename "$rad_path" .nc| sed -e s"/.Smoothed$//" -e s"/^[.]//")
 # construct granule label string for slurm job names
 export SDPC_GRANULE_LABEL="${rad_basename}"
 
+irr_select_window=$(config_setting level1b.irr_select_window)
+
 # Generate file list file on master node
-irr_file=$(select_irr.py "$rad_path")
+irr_file=$(select_irr.py --window $irr_select_window "$rad_path")
 snow_file=$(select_ims.py "$rad_path")
 granule_dir=$(dirname "$rad_path")
 file_list_file="$granule_dir/${rad_basename}.lis"
