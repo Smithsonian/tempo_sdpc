38ce04609b58ecf32efe158f143c737ea5eaeb33
John C. Houck <jhouck@cfa.harvard.edu>
2023-10-12 18:02:31 -0400

    cron: pull_ims.sh should avoid overwriting existing files

--- cron/pull_ims.sh
+++ cron/pull_ims.sh
@@ -33,8 +33,18 @@ EOF
 new_list=$(find $target_dir -name "ims???????_1km_v*.nc.gz")
 
 if test x"$new_list" != x ; then
-   gunzip -f $new_list
-   new_list=$(echo $new_list | sed -e 's,.gz,,g')
-   asdc_files.py --dbfile $rootdir/ims.sqlite --add $new_list
+   kept_list=""
+   for gzfile in $new_list ; do
+       file=$(basename $gzfile .gz)
+       if test -f $target_dir/$file ; then
+          /bin/rm $gzfile
+       else
+          gunzip $gzfile
+          kept_list="$target_dir/$file $kept_list"
+       fi
+   done
+   if test x"$kept_list" != x ; then
+      asdc_files.py --dbfile $rootdir/ims.sqlite --add $kept_list
+   fi
 fi
 
