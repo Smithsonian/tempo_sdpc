b9ed68ee3ba6a0b0a87377a65b835d40fd09c667
John C. Houck <jhouck@cfa.harvard.edu>
2023-10-14 12:31:48 -0400

    Update release notes for v4.1.0 release

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -16,7 +16,7 @@ inrroot="$install_root/inr_r2.3.8"
 otsroot="$install_root/ots"
 s6root="$install_root/skarnet"
 
-prefix="$install_root/sdpc/v4"
+prefix="$install_root/sdpc/v4p1"
 
 ./configure --prefix=$prefix \
             --with-compilers=$compilers \
