eb19a5bbc9e0c51456e3b0280d6c5ecc242a5094
John C. Houck <jhouck@cfa.harvard.edu>
2024-02-14 17:37:57 -0500

    pipe: update default configuration for upcoming release

--- pipe/etc/o3prof_config.sh
+++ pipe/etc/o3prof_config.sh
@@ -1,6 +1,7 @@
 #
 # scene selection:  "all" | "none" | "[n1,n2,n3]"
-o3p_scan_select="[1,4,8,12]"
+#o3p_scan_select="[1,4,8,12]"
+o3p_scan_select="none"
 
 # computer cluster load management
 o3p_num_hosts=6
