f9546ba5dbf0e7cfc4b3165420f526d92a0afb26
John C. Houck <jhouck@cfa.harvard.edu>
2024-04-15 07:11:17 -0400

    pipe: pipe_setup_nodes.sh script supports checking that directories exist

--- pipe/setup/pipe_mkdirs_node.sh
+++ pipe/setup/pipe_mkdirs_node.sh
@@ -3,9 +3,48 @@
 set -u
 set -e
 
+exit_usage()
+{
+  echo "Usage:  $(basename $0) [options] SDPC_PIPE_DIR SDPC_ROOT"
+  echo "Optional arguments:"
+  echo " --help    Print this listing"
+  echo " --check   Check that node directories exist"
+  exit "$1"
+}
+
+if test $# -eq 0 ; then
+   exit_usage 0
+fi
+
+_check=""
+
+# Process optional args
+while [ "$#" != "0" ]
+do
+  case "$1" in
+    --*)
+      case "$1" in
+        --help)
+          exit_usage 0
+          ;;
+        --check)
+          _check="$1"
+          shift;
+          ;;
+        *)
+          echo "Unknown option: $1"
+          exit 1
+          ;;
+      esac
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
 if test $# -ne 2 ; then
-   echo "Usage:  $0  SDPC_PIPE_DIR SDPC_ROOT"
-   exit 0
+   exit_usage 1
 fi
 
 _pipe_dir=$1
@@ -26,17 +65,6 @@ assert_dir_exists()
    fi
 }
 
-assert_dir_absent()
-{
-   dir=$1
-
-   if test -e $dir ; then
-      echo "File exists: $dir"
-      exit 1
-   fi
-}
-
-#assert_dir_absent $SDPC_NODE_DIR
 assert_dir_exists $SDPC_REFDATA_DIR
 assert_dir_exists $SDPC_PIPE_DIR
 assert_dir_exists $SDPC_PIPE_DIR/inr
@@ -52,15 +80,12 @@ mkdirlist()
    done
 }
 
-link_existing_dir()
-{
-  from=$1
-  to=$2
-
-  assert_dir_exists $from
-  ln -s -f -n $from $to
-}
-
 PIPE_NODE_DIRS="L0 L1 L2"
 
-mkdirlist $SDPC_NODE_DIR "$PIPE_NODE_DIRS"
+if test -z "$_check" ; then
+   mkdirlist $SDPC_NODE_DIR "$PIPE_NODE_DIRS"
+else
+   for sub in $PIPE_NODE_DIRS ; do
+       assert_dir_exists $SDPC_NODE_DIR/$sub
+   done
+fi
--- pipe/setup/pipe_setup_nodes.sh
+++ pipe/setup/pipe_setup_nodes.sh
@@ -7,17 +7,69 @@ set -e
 : "${SDPC_ROOT:?SDPC_ROOT not set -- source sdpc_env.sh}"
 : "${SDPC_PIPE_DIR:?SDPC_PIPE_DIR not set -- source sdpc_env.sh}"
 
-if test $# -ne 1 ; then
-   echo "Usage:  $0  <slurm-partition-name>"
-   exit 0
+exit_usage()
+{
+  echo "Usage:  $(basename $0) [options] <slurm-partition-name>"
+  echo "Optional arguments:"
+  echo " --help    Print this listing"
+  echo " --check   Check that node directories exist"
+  exit "$1"
+}
+
+if test $# -eq 0 ; then
+   exit_usage 0
 fi
 
-part_name=$1
+_check=""
+
+# Process optional args
+while [ "$#" != "0" ]
+do
+  case "$1" in
+    --*)
+      case "$1" in
+        --help)
+          exit_usage 0
+          ;;
+        --check)
+          _check="$1"
+          shift;
+          ;;
+        *)
+          echo "Unknown option: $1"
+          exit 1
+          ;;
+      esac
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if test $# -gt 1 ; then
+   exit_usage 1
+elif test $# -eq 1 ; then
+   part_name=$1
+else
+   # use default partition name
+   if test -n "$SLURM_PARTITION" ; then
+      part_name="$SLURM_PARTITION"
+   elif test -n "$SBATCH_PARTITION" ; then
+      part_name="$SBATCH_PARTITION"
+   else
+      part_name=$(sinfo -o "%P" | grep '*' | tr -d '*')
+   fi
+fi
 
 # list of responding compute nodes in this partition
 _node_list=$(sinfo -p "$part_name" -r -h -N -o %n)
 
 for node in $_node_list; do
-  printf "Creating pipeline node directory: $node:$SDPC_NODE_DIR\n"
-  ssh $node $SDPC_ROOT/bin/pipe_mkdirs_node.sh $SDPC_PIPE_DIR $SDPC_ROOT
+  if test -z "$_check" ; then
+     printf "Creating pipeline node directory: $node:$SDPC_NODE_DIR\n"
+  else
+     printf "Checking pipeline node directory: $node:$SDPC_NODE_DIR\n"
+  fi
+  ssh $node $SDPC_ROOT/bin/pipe_mkdirs_node.sh $_check $SDPC_PIPE_DIR $SDPC_ROOT
 done
