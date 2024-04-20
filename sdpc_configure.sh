a0dc6de6162253f05e002aec49dff3d5f4dfccb0
John C. Houck <jhouck@cfa.harvard.edu>
2024-04-19 21:20:18 -0400

    build: increment sdpc version number, INR SW version

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -12,11 +12,11 @@ refdata_dir="$soft_home/refdata"
 ancdata_dir="$archive_home/ancillary"
 
 install_root="$soft_home/install/$build_arch"
-inrroot="$install_root/inr_r2.3.10"
+inrroot="$install_root/inr_r3.0.0"
 otsroot="$install_root/ots"
 s6root="$install_root/skarnet"
 
-prefix="$install_root/sdpc/v4p3"
+prefix="$install_root/sdpc/v4p4"
 
 ./configure --prefix=$prefix \
             --with-compilers=$compilers \
