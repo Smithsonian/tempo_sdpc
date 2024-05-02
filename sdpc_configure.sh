02f654d7b21af36b8507d04bb9dc306d4889f4e6
John C. Houck <jhouck@cfa.harvard.edu>
2024-05-02 17:22:12 -0400

    Update to INR SW R3.0.2

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -12,7 +12,7 @@ refdata_dir="$soft_home/refdata"
 ancdata_dir="$archive_home/ancillary"
 
 install_root="$soft_home/install/$build_arch"
-inrroot="$install_root/inr_r3.0.1"
+inrroot="$install_root/inr_r3.0.2"
 otsroot="$install_root/ots"
 s6root="$install_root/skarnet"
 
