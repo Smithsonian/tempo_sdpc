7dbe33d7b91d624b247f73f1631a50a49b973fcf
John C. Houck <jhouck@cfa.harvard.edu>
2024-10-02 06:55:57 -0400

    update release notes

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -16,7 +16,9 @@ inrroot="$install_root/inr_r3.0.2"
 otsroot="$install_root/ots"
 s6root="$install_root/skarnet"
 
-prefix="$install_root/sdpc/v4p4p2"
+prefix="$install_root/sdpc/v4p4p3"
+
+# --with-iocsdpc=/tempo/nas0/sdpc_soft/src/ots_sdpc.git/iocsdpc_test \
 
 ./configure --prefix=$prefix \
             --with-compilers=$compilers \
