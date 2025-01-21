d9af0e06c7be748bd3c7abea15f58a69215166cc
John C. Houck <jhouck@cfa.harvard.edu>
2025-01-21 08:47:01 -0500

    build: use case statement to select between configurations

--- sdpc_configure.sh
+++ sdpc_configure.sh
@@ -2,23 +2,36 @@
 
 compilers=gnu
 
-# -- HEAD network
-OS_LABEL=rh8
-DEVROOT=/tempo/nas0
-localroot=/scratch
-pythonbindir=/usr/bin
+settings=HEAD
+#settings=ITS
 
-# -- ITS network
-#OS_LABEL=rocky8
-#DEVROOT=/proj
-#localroot=/h
-#pythonbindir=/opt/cfpython/python-3.12/bin
+case "$settings" in
+  ITS )
+     OS_LABEL=rocky8
+     DEVROOT=/proj
+     localroot=/h
+     #pythonbindir=/opt/cfpython/python-3.12/bin
+     pythonbindir=/proj/sdpc_soft/install/gnu-rocky8/python-3.12.8/bin
+     ;;
+
+  HEAD )
+     OS_LABEL=rh8
+     DEVROOT=/tempo/nas0
+     localroot=/scratch
+     pythonbindir=/usr/bin
+     ;;
+
+  * )
+     echo "Unsupported network"
+     exit 1
+     ;;
+esac
 
 build_arch="${compilers}-${OS_LABEL}"
 
 soft_home="$DEVROOT/sdpc_soft"
-archive_home="$DEVROOT/sdpc_archive"
 pipe_home="$DEVROOT/sdpc/liveroot"
+archive_home="$DEVROOT/sdpc_archive"
 
 refdata_dir="$soft_home/refdata"
 ancdata_dir="$archive_home/ancillary"
