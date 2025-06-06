de7dc83fcb093e28ab6522f1035ebcc2cd571839
John C. Houck <jhouck@cfa.harvard.edu>
2025-06-06 19:26:19 -0400

    pipe: interface with updated INR quality check script

--- pipe/src/inr_qa_setup.py
+++ pipe/src/inr_qa_setup.py
@@ -62,28 +62,27 @@ def get_goes_paths (dbfile, t1, t2):
 
     return paths
 
-def classify_goes_file (path):
-    with Dataset (path, "r") as nc:
-        platform_id = nc.getncattr ('platform_ID')
-    return int(platform_id.strip('G'))
-
-def create_config_file_text (work_dir, radiance_dir, goes_dir):
+def create_config_file_text (work_dir, radiance_dir, goes_west_dir, goes_east_dir, terrain_reg):
     """
     The seemingly pointless first line MUST be present,
     and the following lines MUST be in the order specified:
         - Diary_path --> diary_directory
         - granule_path --> granule_directory
-        - ref_im_path --> ref_im_directory
+        - ref_im_left_path --> ref_im_left_directory
+        - ref_im_right_path --> ref_im_right_directory
         - OutputParentFolder --> output_parent_directory
         - TempMatFiles --> temp_MAT_files_directory
+        - terrain_reg --> terrain_reg  (T or F)
     """
     config_file_lines = [\
     "variable_name=variable_path",
     "Diary_path={work_dir}/diary/",
     "granule_path={radiance_dir}/",
-    "ref_im_path={goes_dir}/",
+    "ref_im_left_path={goes_west_dir}/",
+    "ref_im_right_path={goes_east_dir}/",
     "OutputParentFolder={work_dir}/output/",
-    "TempMatFiles={work_dir}/tmp/"]
+    "TempMatFiles={work_dir}/tmp/",
+    "terrain_reg={terrain_reg}"]
     config_file_text = "\n".join (config_file_lines) + "\n"
     return config_file_text.format(**locals())
 
@@ -92,28 +91,27 @@ def make_symlinks (dir, paths):
         dir_p = os.path.join (dir, os.path.basename(p))
         os.symlink (p, dir_p)
 
-def initialize_working_directory (work_dir, rad_paths, goes_paths, goes_west_id, goes_east_id):
+def initialize_working_directory (work_dir, rad_paths, goes_west_paths, goes_east_paths, terrain_reg):
     # populate subdirectory with TEMPO radiances
     radiance_dir = os.path.join (work_dir, "radiances")
     os.mkdir (radiance_dir)
     make_symlinks (radiance_dir, rad_paths)
-    # populate subdirectory with GOES imagery
-    goes_dir = os.path.join (work_dir, "goes")
-    os.mkdir (goes_dir)
-    make_symlinks (goes_dir, goes_paths)
+    # populate subdirectories with GOES East/West imagery
+    goes_west_dir = os.path.join (work_dir, "goes_west")
+    os.mkdir (goes_west_dir)
+    make_symlinks (goes_west_dir, goes_west_paths)
+    goes_east_dir = os.path.join (work_dir, "goes_east")
+    os.mkdir (goes_east_dir)
+    make_symlinks (goes_east_dir, goes_east_paths)
     # create working directories
     for d in ["diary", "output", "tmp"]:
         dir = os.path.join (work_dir, d)
         os.mkdir (dir)
     # generate config file
     config_file_path = os.path.join (work_dir, "config.txt")
-    config_file_text = create_config_file_text (work_dir, radiance_dir, goes_dir)
+    config_file_text = create_config_file_text (work_dir, radiance_dir, goes_west_dir, goes_east_dir, terrain_reg)
     with open (config_file_path, "w") as fp:
         fp.write(config_file_text)
-    with open (os.path.join (work_dir, "GOES_East"), "w") as fp:
-        fp.write(format(goes_east_id))
-    with open (os.path.join (work_dir, "GOES_West"), "w") as fp:
-        fp.write(format(goes_west_id))
     print("initialized working directory: {}".format(work_dir))
 
 def main():
@@ -121,6 +119,7 @@ def main():
     parser.add_argument('--dbfile', help="TEMPO archive sqlite database path")
     parser.add_argument('--dir', help="Target directory path (must exist)")
     parser.add_argument('--scan', default=None, type=int, help="(optional) radiance scan number")
+    parser.add_argument('--terrain_reg', default='T', help="Radiance geolocation is terrain registered [T|F]")
     parser.add_argument('date', help="Date of interest (YYYY-MM-DD)")
     if len(sys.argv)==1:
         parser.print_usage(sys.stderr)
@@ -155,19 +154,15 @@ def main():
     # Collect GOES imagery
     goes_east = "$SDPC_ANCILLARY_ROOT/var/goes/cmieast.sqlite"
     goes_west = "$SDPC_ANCILLARY_ROOT/var/goes/cmiwest.sqlite"
+
     goes_east_paths = get_goes_paths (goes_east, t1, t2)
     if goes_east_paths is None:
         sys.exit(1)
-    goes_east_id = classify_goes_file (goes_east_paths[0])
-
     goes_west_paths = get_goes_paths (goes_west, t1, t2)
     if goes_west_paths is None:
         sys.exit(1)
-    goes_west_id = classify_goes_file (goes_west_paths[0])
 
-    goes_paths = goes_east_paths + goes_west_paths
-
-    initialize_working_directory (work_dir, rad_paths, goes_paths, goes_west_id, goes_east_id)
+    initialize_working_directory (work_dir, rad_paths, goes_west_paths, goes_east_paths, args.terrain_reg)
 
 if __name__ == "__main__":
     main()
